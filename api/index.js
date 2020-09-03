a160f2f677be6454aaf88476f37b9c4f06ab06fa
Bruno De Angelis <brunordeangelis@gmail.com>
2020-09-03 01:51:16 -0300

    feat: register user validation

--- api/index.js
+++ api/index.js
@@ -1,10 +1,79 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const md5 = require("md5");
+// const { Sequelize } = require("sequelize");
+const knex = require("knex")({
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    user: "root",
+    password: "",
+    database: "aoyind3",
+  },
+});
+
+const { body, validationResult } = require("express-validator");
+
 const app = express();
 const router = express.Router();
 
-router.get("/account", async (req, res) => {
-  res.json({
-    res: "you are in /account",
+// const sequelize = new Sequelize("aoyind3", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+app.use(bodyParser.json());
+
+const registerAccountValidations = [
+  body("name")
+    .isLength({
+      min: 3,
+      max: 40,
+    })
+    .withMessage("El nombre debe tener un mínimo de 3 caracteres y un máximo de 40"),
+  body("email").trim().isEmail().normalizeEmail().withMessage("El email debe ser válido"),
+  body("auxEmail").trim().isEmail().normalizeEmail().withMessage("El email debe ser válido"),
+  body("password")
+    .trim()
+    .isLength({
+      min: 5,
+    })
+    .withMessage("La contraseña debe tener un mínimo de 5 caracteres"),
+];
+
+router.post("/accounts", registerAccountValidations, async (req, res) => {
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    return res.status(400).json({ errors: validationErrors.array() });
+  }
+
+  const accountExistsQuery = await knex("cuentas").count("Nombre").where("Nombre", req.body.name);
+  const [accountExists] = Object.values(accountExistsQuery[0]);
+
+  if (!!accountExists) {
+    return res.status(409).json({
+      error: "account_exists",
+    });
+  }
+
+  const emailExistsQuery = await knex("cuentas").count("Email").where("Email", req.body.email);
+  const [emailExists] = Object.values(emailExistsQuery[0]);
+
+  if (!!emailExists) {
+    return res.status(409).json({
+      error: "email_exists",
+    });
+  }
+
+  await knex("cuentas").insert({
+    nombre: req.body.name,
+    password: md5(req.body.password),
+    email: req.body.email,
+    emailAux: req.body.auxEmail,
+  });
+
+  return res.json({
+    status: "success",
   });
 });
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,68 @@
 <template>
   <main>
     <div class="w-full h-screen flex justify-center items-center">
-      <form @submit.prevent="registerUser" class="bg-black p-8">
-        <label for="account-name">Nombre de cuenta</label>
-        <input
-          type="text"
-          name="account-name"
-          id="account-name"
-          class="text-input"
-          required
-          v-model="accountName"
-        />
-        <!-- <Btn>Registrar cuenta</Btn> -->
-        <input type="submit" value="Registrar cuenta" />
+      <form
+        @submit.prevent="registerUser"
+        class="flex flex-col gap-y-4 bg-black p-8"
+        id="main-form"
+      >
+        <div class="flex flex-col">
+          <label for="name">Nombre de cuenta</label>
+          <input type="text" name="name" id="name" class="text-input" required v-model="name" />
+        </div>
+
+        <div class="flex flex-col">
+          <label for="password">Contraseña</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            class="text-input"
+            required
+            v-model="password"
+          />
+        </div>
+
+        <div class="flex flex-col">
+          <label for="repeat-password">Repetir contraseña</label>
+          <input
+            type="password"
+            name="repeat-password"
+            id="repeat-password"
+            class="text-input"
+            required
+            v-model="repeatedPassword"
+          />
+        </div>
+
+        <div class="flex flex-col">
+          <label for="email">Correo electrónico</label>
+          <input type="email" name="email" id="email" class="text-input" required v-model="email" />
+        </div>
+
+        <div class="flex flex-col">
+          <label for="aux-email">Correo electrónico auxiliar</label>
+          <input
+            type="email"
+            name="aux-email"
+            id="aux-email"
+            class="text-input"
+            required
+            v-model="auxEmail"
+          />
+        </div>
+
+        <input type="submit" value="Registrar cuenta" class="bg-gray-900" />
+        <div
+          class="p-4 border"
+          :class="{
+            'border-green-400 bg-green-200 text-green-700': registerStatus == 'success',
+            'border-red-400 bg-red-200 text-red-700': registerStatus == 'error',
+          }"
+          v-show="registerStatus != null"
+        >
+          <p>{{ registerMessage }}</p>
+        </div>
       </form>
     </div>
   </main>
@@ -22,12 +72,47 @@
 export default {
   data() {
     return {
-      accountName: "",
+      name: "",
+      password: "",
+      repeatedPassword: "",
+      email: "",
+      auxEmail: "",
+      registerStatus: null,
+      registerMessage: "",
     };
   },
   methods: {
-    registerUser() {
-      console.log("register!");
+    async registerUser() {
+      const { name, password, repeatedPassword, email, auxEmail } = this;
+      this.registerStatus = null;
+      this.registerMessage = "";
+
+      try {
+        const res = await this.$axios.post("/api/accounts", {
+          name,
+          password,
+          repeatedPassword,
+          email,
+          auxEmail,
+        });
+
+        this.registerStatus = "success";
+        this.registerMessage = "Cuenta creada exitosamente.";
+      } catch (e) {
+        this.registerStatus = "error";
+
+        if (e.response) {
+          const data = e.response.data;
+
+          if (data.error == "account_exists") {
+            this.registerMessage = "Esa cuenta ya existe.";
+          } else if (data.error == "email_exists") {
+            this.registerMessage = "Ya existe una cuenta con ese email.";
+          }
+
+          console.log(data);
+        }
+      }
     },
   },
 };
@@ -39,6 +124,16 @@ body {
 }
 
 .text-input {
-  @apply p-2;
+  @apply p-2 bg-gray-800;
+}
+
+#main-form {
+  @apply w-full;
+}
+
+@screen sm {
+  #main-form {
+    width: 450px;
+  }
 }
 </style>
