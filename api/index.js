a464f8dc5671635647e9905938476108791deabf
Bruno De Angelis <brunordeangelis@gmail.com>
2020-09-09 18:20:38 -0300

    deploy vercel

--- api/index.js
+++ api/index.js
@@ -13,6 +13,11 @@ const knex = require("knex")({
   },
 });
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_NAME);
+
 const { body, validationResult } = require("express-validator");
 
 let transporter;
