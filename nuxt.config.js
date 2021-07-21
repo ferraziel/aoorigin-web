709031775abfedff4960aba868fc088419768275
Bruno De Angelis <brunordeangelis@gmail.com>
2021-07-21 18:29:08 -0300

    change calendar

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,3 @@
-// No Borrar, This is for the fullcalendar.io
-import PnpWebpackPlugin from "pnp-webpack-plugin";
-
 const description =
   "MMORPG 2D gratuito de fantasía medieval, hecho en Argentina. ¡Celebramos 20 años! Creá un personaje, subí de nivel, combatí, trabajá, ¡y viví aventuras únicas con tus amigos!";
 
@@ -50,8 +47,7 @@ export default {
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
         rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css2?family=Cardo:wght@400;700&family=Livvic:ital,wght@0,400;0,600;1,400&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Cardo:wght@400;700&family=Livvic:ital,wght@0,400;0,600;1,400&display=swap",
       },
       {
         rel: "stylesheet",
@@ -178,15 +174,4 @@ export default {
   //   gzip: true,
   //   routes: createSitemapRoutes,
   // },
-  
-  // This is for the fullcalendar.io
-  build: {
-    transpile: /@fullcalendar.*/, // always needed
-
-    // this `extend` callback is only needed if using Yarn Plug-n-Play
-    extend(config) {
-      config.resolve.plugins.push(PnpWebpackPlugin);
-      config.resolveLoader.plugins.push(PnpWebpackPlugin.moduleLoader(module));
-    }
-  }
 };
