98047a420a1268c74ab1d0054d09fdf9671dc6b3
Bruno De Angelis <brunordeangelis@gmail.com>
2021-07-21 18:15:11 -0300

    reoder nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
-  <header class="w-full fixed z-50 top-0 left-0">
-    <nav class="bg-black bg-opacity-75 py-2 w-full">
-      <div class="container flex w-full justify-between items-center">
+  <header class="fixed top-0 left-0 z-50 w-full">
+    <nav class="w-full py-2 bg-black bg-opacity-75">
+      <div class="container flex items-center justify-between w-full">
         <NuxtLink to="/" class="flex items-center gap-2 main-title flex-shrink-1" style="z-index: 999">
           <img src="@/assets/img/logo_square.png" alt="Logo Argentum 20 cuadrado" class="w-12" />
-          <span class="text-primary text-2xl hidden lg:block">Argentum 20</span>
+          <span class="hidden text-2xl text-primary lg:block">Argentum 20</span>
         </NuxtLink>
 
         <div class="flex items-center gap-x-16">
-          <ul class="hidden lg:flex items-center gap-x-10 uppercase tracking-wide text-gray-300">
+          <ul class="items-center hidden tracking-wide text-gray-300 uppercase lg:flex gap-x-10">
             <li v-for="link in links" :key="link.route">
               <NuxtLink :to="link.route" class="inline-block py-2 hover:text-gray-100">{{ link.label }}</NuxtLink>
             </li>
@@ -17,34 +17,22 @@
                 >Manual</a
               >
             </li>
-            <li>
-              <a
-                href="https://www.elmesonhostigado.com/foro/"
-                target="_blank"
-                class="inline-block py-2 hover:text-gray-100"
-                >Foro</a
-              >
-            </li>
           </ul>
 
           <div class="flex items-center gap-x-4">
-            <NuxtLink to="/#juga-gratis" class="hidden lg:inline-block btn btn-silver border px-2 py-1 text-sm"
-              >Descargar</NuxtLink
-            >
-
-            <p class="uppercase tracking-wide text-sm hidden lg:block">
+            <p class="hidden text-sm tracking-wide uppercase lg:block">
               <span v-if="$store.state.isGameOnline">{{ $store.state.onlineCount || 0 }} usuarios online</span>
-              <span v-else class="text-white p-2 bg-red-500 font-bold">Servidor Offline</span>
+              <span v-else class="p-2 font-bold text-white bg-red-500">Servidor Offline</span>
             </p>
 
             <NuxtLink to="/cuenta" v-if="$auth.loggedIn">Mi cuenta</NuxtLink>
-            <NuxtLink v-else to="/login" class="btn btn-silver border px-2 py-1 text-sm">Cuenta</NuxtLink>
+            <NuxtLink v-else to="/login" class="px-2 py-1 text-sm border btn btn-silver">Cuenta</NuxtLink>
           </div>
         </div>
 
         <button
           @click="showMobileMenu = !showMobileMenu"
-          class="lg:hidden pl-4 py-4 focus:outline-none"
+          class="py-4 pl-4 lg:hidden focus:outline-none"
           style="z-index: 999"
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-5">
@@ -61,9 +49,9 @@
     <transition name="mobile-menu">
       <nav
         v-show="showMobileMenu"
-        class="h-screen bg-gray-1000 w-full fixed top-0 left-0 flex flex-col justify-center overflow-y-scroll"
+        class="fixed top-0 left-0 flex flex-col justify-center w-full h-screen overflow-y-scroll bg-gray-1000"
       >
-        <ul class="text-3xl uppercase tracking-wider">
+        <ul class="text-3xl tracking-wider uppercase">
           <li v-for="(link, i) in links" :key="i" @click="showMobileMenu = false">
             <NuxtLink :to="link.route" class="mobile-nav-link" :class="{ 'border-t': i == 0 }">{{
               link.label
@@ -71,9 +59,6 @@
           </li>
           <li>
             <a href="https://wiki.ao20.com.ar/es/home" target="_blank" class="mobile-nav-link">Manual</a>
-          </li>
-          <li>
-            <a href="https://www.elmesonhostigado.com/foro/" target="_blank" class="mobile-nav-link">Foro</a>
           </li>
         </ul>
       </nav>
@@ -88,6 +73,10 @@ export default {
       showMobileMenu: false,
       links: [
         {
+          label: "Jugá gratis",
+          route: "/#juga-gratis",
+        },
+        {
           label: "Noticias",
           route: "/noticias",
         },
@@ -95,14 +84,6 @@ export default {
           label: "Ranking",
           route: "/ranking",
         },
-        {
-          label: "Staff",
-          route: "/staff",
-        },
-        // {
-        //   label: "Calendario Eventos",
-        //   route: "/calendario",
-        // },
       ],
     };
   },
