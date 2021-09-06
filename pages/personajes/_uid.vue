b14de5a7cf08f74941a7891d67c1c8958dabfec9
Lucas Recoaro <lucas.recoaro@gmail.com>
2021-09-06 22:29:13 +1200

    Agrego posibilidad de ver todos los personajes borrados de la cuenta

--- pages/personajes/_uid.vue
+++ pages/personajes/_uid.vue
@@ -9,20 +9,35 @@
       <h3>Setear Wallet Id para Personaje con Metamask</h3>
       <h3>WalletId: {{ character.eth_wallet_id }}</h3>
 
-      <CharacterRenderer  :isDead="character.min_hp == 0"
-                          :bodyData="character.bodyGraphicData"
-                          :helmetData="character.helmetGraphicData"
-                          :weaponData="character.weaponGraphicData"
-                          :shieldData="character.shieldGraphicData"
-                          :headData="character.headGraphicData"
-                          background="https://i1.sndcdn.com/artworks-wclS76qZZbYHAhoX-yVPKnw-t500x500.jpg"
+      <CharacterRenderer
+        :isDead="character.min_hp == 0"
+        :bodyData="character.bodyGraphicData"
+        :helmetData="character.helmetGraphicData"
+        :weaponData="character.weaponGraphicData"
+        :shieldData="character.shieldGraphicData"
+        :headData="character.headGraphicData"
+        background="https://i1.sndcdn.com/artworks-wclS76qZZbYHAhoX-yVPKnw-t500x500.jpg"
       />
 
-      <button @click="addWalletIdToCharacter()" v-if="!character.eth_wallet_id" type="submit" class="btn btn-silver self-start">
+      <button
+        @click="addWalletIdToCharacter()"
+        v-if="!character.eth_wallet_id && !($dayjs(character.deleted_at).year() > 1970)"
+        type="submit"
+        class="btn btn-silver self-start"
+      >
         Agregar Wallet seleccionada en Personaje.
       </button>
-
       <MessageBox :status="addWalletStatus" :message="addWalletMessage" />
+
+      <button
+        @click="recoverCharacter()"
+        v-if="$dayjs(character.deleted_at).year() > 1970"
+        type="submit"
+        class="btn btn-silver self-start"
+      >
+        Recuperar Personaje
+      </button>
+
     </div>
 
     <section v-else class="text-center mt-24">
@@ -46,35 +61,42 @@ export default {
       userId: params.uid,
       character,
       addWalletMessage: "",
-      addWalletStatus: null
+      addWalletStatus: null,
     };
   },
   async mounted() {
-      if (!ethereum) {
-        alert("Necesitas Metamask para poder poner la wallet en tu personaje.");
-        console.log("Non-Ethereum browser detected. You should consider trying MetaMask!");
-      }
+    if (!ethereum) {
+      alert("Necesitas Metamask para poder poner la wallet en tu personaje.");
+      console.log("Non-Ethereum browser detected. You should consider trying MetaMask!");
+    }
   },
 
   methods: {
     async addWalletIdToCharacter() {
-        if (confirm("Estas seguro que quieres agregar la wallet seleccionada en tu personaje? Esta operacion no podra ser modificada.")) {
-          this.addWalletStatus = "PENDING";
-          this.addWalletMessage = "Confirme la asignacion de billetera mediante metamask";
+      if (confirm("Estas seguro que quieres agregar la wallet seleccionada en tu personaje? Esta operacion no podra ser modificada.")) {
+        this.addWalletStatus = "PENDING";
+        this.addWalletMessage = "Confirme la asignacion de billetera mediante metamask";
 
-          const ethWallets = await ethereum.request({ method: 'eth_requestAccounts' });
+        const ethWallets = await ethereum.request({ method: "eth_requestAccounts" });
 
-          try {
-            await this.$axios.$put(`/characters/addWalletIdInCharacter/${this.userId}/${ethWallets[0]}`)
-            this.addWalletStatus = "OK";
-            this.addWalletMessage = "La billetera fue asignada correctamente al personaje. Ahora podras usar tus NFT en el juego.";
+        try {
+          await this.$axios.$put(`/characters/addWalletIdInCharacter/${this.userId}/${ethWallets[0]}`);
+          this.addWalletStatus = "OK";
+          this.addWalletMessage =
+            "La billetera fue asignada correctamente al personaje. Ahora podras usar tus NFT en el juego.";
 
-            this.character.eth_wallet_id = ethWallets[0];
-          } catch (error) {
-            this.addWalletStatus = "ERROR";
-            this.addWalletMessage = error.response.data.message;
-          }
+          this.character.eth_wallet_id = ethWallets[0];
+        } catch (error) {
+          this.addWalletStatus = "ERROR";
+          this.addWalletMessage = error.response.data.message;
         }
+      }
+    },
+
+    async recoverCharacter() {
+      if (confirm("Estas seguro que quieres recuperar a tu personaje?.")) {
+        alert('no implementado aun.')
+      }
     },
   },
 
--- pages/personajes/index.vue
+++ pages/personajes/index.vue
@@ -46,6 +46,52 @@
         <p class="text-2xl">Aún no hay personajes en la cuenta.</p>
       </section>
     </div>
+
+    <div class="text-center mb-12">
+      <h1 class="section-title">Personajes Borrados</h1>
+      <h5>Si queres podes recuperar tus personajes borrados por dinero</h5>
+      <ul
+        v-if="deletedCharacters.length"
+        class="max-w-screen-md mx-auto bg-gray-900 border-2 border-gr border-gr-primary p-4 md:p-6"
+      >
+        <NuxtLink
+          v-for="deletedCharacters in deletedCharacters"
+          :key="deletedCharacters.id"
+          :id="deletedCharacters.id"
+          :to="`/personajes/${deletedCharacters.id}`"
+        >
+          <!-- <CharacterHead :id="deletedCharacters.head_id" :scale="i === 0 ? 5 : i === 1 ? 3 : 2" class="flex-shrink-0 mr-4" /> -->
+
+          <div class="flex flex-col items-center gap-y-2">
+            <div class="flex items-center justify-center border-2 border-gr border-gr-primary p-12 bg-gray-900">
+              <CharacterRenderer
+                :isDead="deletedCharacters.min_hp == 0"
+                :bodyData="deletedCharacters.bodyGraphicData"
+                :helmetData="deletedCharacters.helmetGraphicData"
+                :weaponData="deletedCharacters.weaponGraphicData"
+                :shieldData="deletedCharacters.shieldGraphicData"
+                :headData="deletedCharacters.headGraphicData"
+                background="https://i1.sndcdn.com/artworks-wclS76qZZbYHAhoX-yVPKnw-t500x500.jpg"
+              />
+            </div>
+          </div>
+
+          <h2 class="text-4xl text-gr gr-gold">{{ deletedCharacters.name }}</h2>
+          <h2>Nivel: {{ deletedCharacters.level }}</h2>
+          <h2>Ultimo login: {{ $dayjs(deletedCharacters.fecha_ingreso).format("DD [de] MMMM [de] YYYY [a las] HH:mm") }}</h2>
+          <h2>WalletId: {{ deletedCharacters.eth_wallet_id }}</h2>
+          <h2>Online: {{ deletedCharacters.is_logged }}</h2>
+          <hr />
+          <br />
+        </NuxtLink>
+
+
+      </ul>
+
+      <section v-else class="text-center mt-24">
+        <p class="text-2xl">Aún no hay personajes borrados en la cuenta.</p>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -55,6 +101,7 @@ export default {
   async asyncData({ $axios }) {
     return {
       characters: await $axios.$get(`characters`),
+      deletedCharacters: await $axios.$get(`characters/getAllDeletedCharacters`),
     };
   },
 };
