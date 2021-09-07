d47941797f292ec602980ede787e81fc44bd1487
Lucas Recoaro <lucas.recoaro@gmail.com>
2021-09-07 16:56:23 +1200

    Aprolijo un poco la seccion de personaje

--- components/UserAndItemsRenderer.vue
+++ components/UserAndItemsRenderer.vue
@@ -2,46 +2,58 @@
 <template>
   <div>
     <img :src="user.canvasImage" class="" />
-    <h7>{{ user.description }}</h7>
+    <h6>{{ user.description }}</h6>
+    <div style="display: table-cell; vertical-align: middle">
+      <h4>Informacion del personaje</h4>
+      <ul>
+        <li>Fecha creacion: {{ $dayjs(user.created_at).format("DD [de] MMMM [de] YYYY [a las] HH:mm") }}</li>
+        <li>Nivel: {{ user.level }}</li>
+        <li>Experiencia: {{ user.exp }}</li>
+        <li>Sexo: {{ user.genre_id }}</li>
+        <li>Clase: {{ user.class_id }}</li>
+        <li>Max HP: {{ user.max_hp }}</li>
+        <li>Max Stamina: {{ user.max_sta }}</li>
+        <li>Max Mana: {{ user.max_man }}</li>
+        <li>Min Golpe: {{ user.min_hit }}</li>
+        <li>Max Golpe: {{ user.max_hit }}</li>
+        <li>Oro en Inventario: {{ user.gold }}</li>
+        <li>Oro en Banco: {{ user.bank_gold }}</li>
+        <li>Npcs Matados: {{ user.killed_npcs }}</li>
+        <li>Usuarios Matados: {{ user.killed_users }}</li>
+        <li>Ciudadanos Matados: {{ user.ciudadanos_matados }}</li>
+        <li>Criminales Matados: {{ user.criminales_matados }}</li>
+        <li>Nivel de inventario: {{ user.invent_level }}</li>
+        <li>Mascotas salvadas: {{ user.pets_saved }}</li>
+        <!-- <li>Penas: {{ user.counter_pena }}</li> -->
+        <!-- <li>Pertenece a Legion del Caos: {{ user.pertenece_real }}</li> -->
+        <!-- <li>Recibio Armadura Legion del Caos: {{ user.recibio_armadura_caos }}</li> -->
+        <!-- <li>Pertenece a Armada Real: {{ user.pertenece_real }}</li> -->
+        <!-- <li>Recibio Armadura Real: {{ user.recibio_armadura_real }}</li> -->
+        <li>Pertenece a clan: {{ user.guild_index }}</li>
+        <!-- <li>Lista de clanes a los cual pertenecio: {{ user.guild_member_history }}</li> -->
+      </ul>
+    </div>
 
-    <h4>Informacion del personaje</h4>
-    <ul>
-      <li>Fecha creacion: {{ $dayjs(user.created_at).format("DD [de] MMMM [de] YYYY [a las] HH:mm") }}</li>
-      <li>Nivel: {{ user.level }}</li>
-      <li>Experiencia: {{ user.exp }}</li>
-      <li>Sexo: {{ user.genre_id }}</li>
-      <li>Clase: {{ user.class_id }}</li>
-      <li>Max HP: {{ user.max_hp }}</li>
-      <li>Max Stamina: {{ user.max_sta }}</li>
-      <li>Max Mana: {{ user.max_man }}</li>
-      <li>Min Golpe: {{ user.min_hit }}</li>
-      <li>Max Golpe: {{ user.max_hit }}</li>
-      <li>Oro en Inventario: {{ user.gold }}</li>
-      <li>Oro en Banco: {{ user.bank_gold }}</li>
-      <li>Npcs Matados: {{ user.killed_npcs }}</li>
-      <li>Usuarios Matados: {{ user.killed_users }}</li>
-      <li>Ciudadanos Matados: {{ user.ciudadanos_matados }}</li>
-      <li>Criminales Matados: {{ user.criminales_matados }}</li>
-      <li>Nivel de inventario: {{ user.invent_level }}</li>
-      <li>Mascotas salvadas: {{ user.pets_saved }}</li>
-      <!-- <li>Penas: {{ user.counter_pena }}</li> -->
-      <!-- <li>Pertenece a Legion del Caos: {{ user.pertenece_real }}</li> -->
-      <!-- <li>Recibio Armadura Legion del Caos: {{ user.recibio_armadura_caos }}</li> -->
-      <!-- <li>Pertenece a Armada Real: {{ user.pertenece_real }}</li> -->
-      <!-- <li>Recibio Armadura Real: {{ user.recibio_armadura_real }}</li> -->
-      <li>Pertenece a clan: {{ user.guild_index }}</li>
-      <!-- <li>Lista de clanes a los cual pertenecio: {{ user.guild_member_history }}</li> -->
-    </ul>
+    <div style="display: table-cell; vertical-align: middle">
+      <br />
+      <h4>Atributos</h4>
+      <ul>
+        <li>Fuerza: {{ user.attributes.strength }}</li>
+        <li>Agilidad: {{ user.attributes.agility }}</li>
+        <li>Inteligencia: {{ user.attributes.intelligence }}</li>
+        <li>Constitucion: {{ user.attributes.constitution }}</li>
+        <li>Carisma: {{ user.attributes.charisma }}</li>
+      </ul>
+    </div>
 
-    <br />
-    <h4>Atributos</h4>
-    <ul>
-      <li>Fuerza: {{ user.attributes.strength }}</li>
-      <li>Agilidad: {{ user.attributes.agility }}</li>
-      <li>Inteligencia: {{ user.attributes.intelligence }}</li>
-      <li>Constitucion: {{ user.attributes.constitution }}</li>
-      <li>Carisma: {{ user.attributes.charisma }}</li>
-    </ul>
+    <div style="display: table-cell; vertical-align: middle">
+      <br />
+      <h4>Skillpoints</h4>
+      <h6>Puntos de habilidad libres: {{ user.free_skillpoints }}</h6>
+      <ul>
+        <li v-for="(value, name) in user.skillpoints" :key="name" :id="name">{{ name }}: {{ value }}</li>
+      </ul>
+    </div>
 
     <br />
     <h4>Hechizos</h4>
@@ -55,16 +67,6 @@
     <div v-else class="text-center mt-24">
       <p class="text-2xl">No hay hechizos.</p>
     </div>
-
-    <br />
-    <h4>Skillpoints</h4>
-    <h7>Puntos de habilidad libres: {{ user.free_skillpoints }}</h7>
-    <ul>
-      <li v-for="(value, name) in user.skillpoints" :key="name" :id="name">
-        {{ name }}: {{ value }}
-      </li>
-    </ul>
-
 
     <h2>Items de inventario</h2>
     <div v-if="user.inventoryItem.length > 0" lass="text-center mb-12">
