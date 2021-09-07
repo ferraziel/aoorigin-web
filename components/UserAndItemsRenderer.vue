9d8c556055bd0272b8e07d95e74f9e65bb0f49cd
Lucas Recoaro <lucas.recoaro@gmail.com>
2021-09-07 16:27:50 +1200

    Ahora se muestran Atributos y Skillpoints en la pagina de usuario

--- components/UserAndItemsRenderer.vue
+++ components/UserAndItemsRenderer.vue
@@ -6,6 +6,7 @@
 
     <h4>Informacion del personaje</h4>
     <ul>
+      <li>Fecha creacion: {{ $dayjs(user.created_at).format("DD [de] MMMM [de] YYYY [a las] HH:mm") }}</li>
       <li>Nivel: {{ user.level }}</li>
       <li>Experiencia: {{ user.exp }}</li>
       <li>Sexo: {{ user.genre_id }}</li>
@@ -17,20 +18,29 @@
       <li>Max Golpe: {{ user.max_hit }}</li>
       <li>Oro en Inventario: {{ user.gold }}</li>
       <li>Oro en Banco: {{ user.bank_gold }}</li>
-      <li>Puntos de habilidad libres: {{ user.free_skillpoints }}</li>
       <li>Npcs Matados: {{ user.killed_npcs }}</li>
       <li>Usuarios Matados: {{ user.killed_users }}</li>
       <li>Ciudadanos Matados: {{ user.ciudadanos_matados }}</li>
       <li>Criminales Matados: {{ user.criminales_matados }}</li>
       <li>Nivel de inventario: {{ user.invent_level }}</li>
       <li>Mascotas salvadas: {{ user.pets_saved }}</li>
-      <li>Penas: {{ user.counter_pena }}</li>
-      <li>Pertenece a Legion del Caos: {{ user.pertenece_real }}</li>
-      <li>Recibio Armadura Legion del Caos: {{ user.recibio_armadura_caos }}</li>
-      <li>Pertenece a Armada Real: {{ user.pertenece_real }}</li>
-      <li>Recibio Armadura Real: {{ user.recibio_armadura_real }}</li>
+      <!-- <li>Penas: {{ user.counter_pena }}</li> -->
+      <!-- <li>Pertenece a Legion del Caos: {{ user.pertenece_real }}</li> -->
+      <!-- <li>Recibio Armadura Legion del Caos: {{ user.recibio_armadura_caos }}</li> -->
+      <!-- <li>Pertenece a Armada Real: {{ user.pertenece_real }}</li> -->
+      <!-- <li>Recibio Armadura Real: {{ user.recibio_armadura_real }}</li> -->
       <li>Pertenece a clan: {{ user.guild_index }}</li>
-      <li>Lista de clanes a los cual pertenecio: {{ user.guild_member_history }}</li>
+      <!-- <li>Lista de clanes a los cual pertenecio: {{ user.guild_member_history }}</li> -->
+    </ul>
+
+    <br />
+    <h4>Atributos</h4>
+    <ul>
+      <li>Fuerza: {{ user.attributes.strength }}</li>
+      <li>Agilidad: {{ user.attributes.agility }}</li>
+      <li>Inteligencia: {{ user.attributes.intelligence }}</li>
+      <li>Constitucion: {{ user.attributes.constitution }}</li>
+      <li>Carisma: {{ user.attributes.charisma }}</li>
     </ul>
 
     <br />
@@ -45,6 +55,16 @@
     <div v-else class="text-center mt-24">
       <p class="text-2xl">No hay hechizos.</p>
     </div>
+
+    <br />
+    <h4>Skillpoints</h4>
+    <h7>Puntos de habilidad libres: {{ user.free_skillpoints }}</h7>
+    <ul>
+      <li v-for="(value, name) in user.skillpoints" :key="name" :id="name">
+        {{ name }}: {{ value }}
+      </li>
+    </ul>
+
 
     <h2>Items de inventario</h2>
     <div v-if="user.inventoryItem.length > 0" lass="text-center mb-12">
