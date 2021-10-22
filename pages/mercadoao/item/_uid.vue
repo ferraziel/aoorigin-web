07c235508c19ca60b4348adacb96518b2ac9886b
Lucas Recoaro <lucas.recoaro@gmail.com>
2021-10-23 00:33:45 +1300

    Dejo totalmente funcional mercadoao con bnb y aolb

--- pages/mercadoao/item/_uid.vue
+++ pages/mercadoao/item/_uid.vue
@@ -3,7 +3,6 @@
     <PagePadding />
 
     <div v-if="item" lass="text-center mb-12">
-
       <h1 class="section-title">{{ item.Data.NAME }}</h1>
 
       <div class="flex items-center justify-center border-2 border-gr border-gr-primary p-12 bg-gray-900">
@@ -15,10 +14,11 @@
         <li style="color: green">Precio: {{ item.price_in_tokens }} AO Tokens</li>
       </ul>
 
-      <button @click="prepareOrder()"
-              v-if="$auth.loggedIn && usersWithFreeSlots.length <= 0"
-              type="submit"
-              class="btn btn-silver self-start"
+      <button
+        @click="prepareOrder()"
+        v-if="$auth.loggedIn && usersWithFreeSlots.length <= 0"
+        type="submit"
+        class="btn btn-silver self-start"
       >
         Seleccionar personaje al cual asignar item.
       </button>
@@ -28,10 +28,31 @@
         <h1>Para que personaje queres comprar el item?</h1>
 
         <div v-for="user in usersWithFreeSlots" :key="user.name" :id="user.id">
-          <button @click="buyItem(user.id)" class="section-title">{{ user.name }}</button>
+          <h1 @click="selectCharacter(user.id)"
+              class="section-title">
+              {{ user.name }}
+              <span v-if="selectedUserId == user.id">✔️</span>
+          </h1>
         </div>
-      </div>
+        <br>
 
+        <table v-if="selectedUserId" style="margin-left:auto;margin-right:auto;">
+          <h3>Pagar Con:</h3>
+          <tr>
+              <th style="color: cyan">AOLB (AO Libre Token B)</th>
+              <th style="color: yellow">BNB (Binance Token)</th>
+          </tr>
+          <tr>
+              <td>
+                  <img @click="buyItem()" src="https://argentumonline.org/assets/images/ao-libre-aolb-logo.png" class="w-32 h-32 rounded-full mb-4" />
+              </td>
+              <td>
+                  <img @click="buyItemWithNativeToken()" src="https://assets.trustwalletapp.com/blockchains/binance/info/logo.png" class="w-32 h-32 rounded-full mb-4" />
+              </td>
+          </tr>
+        </table>
+
+      </div>
     </div>
 
     <section v-else class="text-center mt-24">
@@ -41,14 +62,20 @@
 </template>
 
 <script>
+// import abi from "@/assets/contracts/0xEA17E48C988D64e92d64550C787B17281F61828e.json";
+import abi from "@/assets/0xEA17E48C988D64e92d64550C787B17281F61828e.json";
+import Web3 from 'web3'
 
 export default {
   async asyncData({ $axios, params }) {
     return {
       item: await $axios.$get(`market/getItemOnSaleById/${params.uid}`),
+      selectedUserId: null,
       usersWithFreeSlots: [],
       buyItemMessage: "",
       buyItemStatus: null,
+      aolbContractAddress: process.env.TOKEN_AOLB_CONTRACT_ADDRESS,
+      abi
     };
   },
 
@@ -64,35 +91,99 @@ export default {
       this.buyItemStatus = "PENDING";
       this.buyItemMessage = "Selecciona el personaje al cual quieras asignar el item.";
 
-      this.$axios.$post(`/users/getUserFromAccountWithFreeSlotsInBankInventory`)
-      .then((data) => {
-        this.buyItemStatus = "PENDING";
-        this.buyItemMessage = "Debes de seleccionar el personaje a comprar el item..";
-        this.usersWithFreeSlots = data.usersWithFreeSlots;
-      })
-      .catch((error) => {
-        this.buyItemStatus = "ERROR";
-        this.buyItemMessage = error.response.data.message;
-      });
-
+      this.$axios
+        .$post(`/users/getUserFromAccountWithFreeSlotsInBankInventory`)
+        .then((data) => {
+          this.buyItemStatus = "PENDING";
+          this.buyItemMessage = "Debes de seleccionar el personaje a comprar el item..";
+          this.usersWithFreeSlots = data.usersWithFreeSlots;
+        })
+        .catch((error) => {
+          this.buyItemStatus = "ERROR";
+          this.buyItemMessage = error.response.data.message;
+        });
     },
 
-    async buyItem(userId) {
-      if (confirm("Estas seguro que quieres comprar este item?.")) {
+    selectCharacter(userId) {
+      this.selectedUserId = userId;
+    },
 
+    async buyItem() {
+      if (confirm("Estas seguro que quieres comprar este item?.")) {
         await ethereum.enable();
 
-        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        console.log("Comprando con AOLB Token.");
+        this.buyItemStatus = "PENDING";
+        this.buyItemMessage =
+          "Esperando aprobar transaccion. Esto puede tardar varios minutos dependiendo la congestion de la red Binance Smart Chain";
+
+        var web3 = new Web3(window.ethereum);
+        const aolTokenContract = new web3.eth.Contract(this.abi, this.aolbContractAddress);
+
+        try {
+          const priceInWei = Web3.utils.toWei(this.item.price_in_tokens.toString(), 'ether')
+          const accounts = await ethereum.request({ method: "eth_accounts" });
+
+          const estimatedGas = await aolTokenContract.methods.transfer(accounts[0], priceInWei).estimateGas({
+            from: accounts[0],
+          });
+
+          console.log("Estimated gas: " + estimatedGas);
+
+          aolTokenContract.methods
+            .transfer(accounts[0], priceInWei)
+            .send({
+                from: accounts[0],
+                gas: estimatedGas
+            })
+            .then((data) => {
+              debugger
+              // Handle the result
+              console.log(data.transactionHash);
+
+              this.$axios
+                .$post(`/market/buyItemMao`, {
+                  itemId: this.item.item_id,
+                  itemQuantity: 1,
+                  userId: this.selectedUserId,
+                  txHash: data.transactionHash,
+                })
+                .then((response) => {
+                  this.buyItemStatus = "OK";
+                  this.buyItemMessage = "Tu pedido ingreso a nuestro sistema con exito, espera a que se confirme la transaccion para que se deposite el item en tu boveda del banco.";
+                  this.usersWithFreeSlots.length = 0
+                })
+                .catch((error) => {
+                  this.buyItemStatus = "ERROR";
+                  this.buyItemMessage = error.response.data.message;
+                });
+            })
+            .catch((error) => {
+              this.buyItemStatus = "ERROR";
+              this.buyItemMessage = error.response.data.message;
+            });
+        } catch (e) {
+          this.buyItemStatus = "ERROR";
+          this.buyItemMessage = e.message;
+        }
+      }
+    },
+
+    async buyItemWithNativeToken() {
+      if (confirm("Estas seguro que quieres comprar este item?.")) {
+        await ethereum.enable();
+
+        const accounts = await ethereum.request({ method: "eth_accounts" });
 
         this.buyItemStatus = "PENDING";
         this.buyItemMessage = "Esperando aprobar transaccion. Esto puede tardar varios minutos dependiendo la congestion de la red Binance Smart Chain";
 
         try {
           const transactionHash = await ethereum.request({
-            method: 'eth_sendTransaction',
+            method: "eth_sendTransaction",
             params: [
               {
-                to: '0x94f5bA56B06a6097f25D6b658f7abE2f78880B79',
+                to: "0x94f5bA56B06a6097f25D6b658f7abE2f78880B79",
                 value: "10000000000000",
                 from: accounts[0],
               },
@@ -102,21 +193,22 @@ export default {
           // Handle the result
           console.log(transactionHash);
 
-          this.$axios.$post(`/market/buyItemMao`, {
-            itemId: this.item.item_id,
-            itemQuantity: 1,
-            userId: userId,
-            txHash: transactionHash,
-          })
-          .then((data) => {
-            this.buyItemStatus = "OK";
-            this.buyItemMessage = "Tu pedido ingreso a nuestro sistema con exito, espera a que se confirme la transaccion para que se deposite el item en tu boveda del banco.";
-          })
-          .catch((error) => {
-            this.buyItemStatus = "ERROR";
-            this.buyItemMessage = error.response.data.message;
-          });
-
+          this.$axios
+            .$post(`/market/buyItemMao`, {
+              itemId: this.item.item_id,
+              itemQuantity: 1,
+              userId: this.selectedUserId,
+              txHash: transactionHash,
+            })
+            .then((data) => {
+              this.buyItemStatus = "OK";
+              this.buyItemMessage = "Tu pedido ingreso a nuestro sistema con exito, espera a que se confirme la transaccion para que se deposite el item en tu boveda del banco.";
+              this.usersWithFreeSlots.length = 0
+            })
+            .catch((error) => {
+              this.buyItemStatus = "ERROR";
+              this.buyItemMessage = error.response.data.message;
+            });
         } catch (error) {
           console.error(error);
           this.buyItemStatus = "ERROR";
@@ -124,7 +216,6 @@ export default {
         }
       }
     },
-
   },
 
   head() {
