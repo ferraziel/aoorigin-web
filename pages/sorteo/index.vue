43fcc95949d003e55a0712d503aacf59a5a36f01
Lucas Recoaro <lucas.recoaro@gmail.com>
2021-11-04 20:13:22 +1300

    Added nfts and raffle section

---
+++ pages/sorteo/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container">
+    <br>
+    <a class="e-widget no-button" href="https://gleam.io/8eyJI/ao-20-nft-giveaway" rel="nofollow">AO 20 NFT Giveaway</a>
+    <script type="text/javascript" src="https://widget.gleamjs.io/e.js" async="true"></script>
+    <meta property="og:url" content="https://gleam.io/8eyJI/ao-20-nft-giveaway"/>
+    <meta property="og:title" content="AO20 NFTs Giveaway">
+    <meta property="twitter:card" content="summary"/>
+    <meta property="fb:app_id" content="152351391599356"/>
+    <meta property="og:description" content="We are delighted to give away 8 of our the first NFTs ever made for Argentum Online Please proceed to follow us on our social networks to earn many entries in the promotion Also, 2 &quot;Matadrakos&quot; will be raffled to be used in the game. ⚔️ 🛡️ Estamos encantados de regalar 8 de nuestros primeros NFT jamás hechos para Argentum Online Proceda a seguirnos en nuestras redes sociales para ganar muchas entradas en la promoción. Tambien se sortearan 2 &quot;Matadrakos&quot; para poder utilizar dentro del juego. https://www.ao20.com.ar - Play the game. Buy/Comprar: http://nfts.ao20.com.ar">
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      news: []
+    }
+  }
+};
+</script>
+
+<style>
+.news_body {
+  @apply text-gray-200;
+}
+
+.news_body h1,
+.news_body h2,
+.news_body h3,
+.news_body h4,
+.news_body h5,
+.news_body h6 {
+  @apply mb-4 mt-6 md:mb-8 md:mt-12;
+}
+
+.news_body h1,
+.news_body h2,
+.news_body h3 {
+  @apply border-b border-gray-800;
+}
+
+.news_body h4,
+.news_body h5,
+.news_body h6 {
+  @apply text-xl mb-2 mt-6 md:mb-4 md:mt-8;
+}
+
+.news_body h1 {
+  @apply text-3xl md:text-4xl pb-4 border-b border-gray-800;
+}
+
+.news_body h2 {
+  @apply text-2xl md:text-3xl pb-3 border-b border-gray-800;
+}
+
+.news_body h3 {
+  @apply text-xl md:text-2xl pb-3 border-b border-gray-800;
+}
+
+.news_body p {
+  @apply my-2 md:my-3;
+}
+
+.news_body a {
+  @apply text-primary underline;
+}
+
+.news ul {
+  list-style-type: disc;
+}
+
+.news ol {
+  list-style-type: decimal;
+}
+
+.news_body > ul,
+.news_body > ol {
+  @apply my-6;
+}
+
+.news ul,
+.news ol {
+  @apply pl-4;
+}
+
+.news ul > li,
+.news ol > li {
+  @apply pl-1;
+}
+
+.news ul > li::marker,
+.news ol > li::marker {
+  @apply text-gray-500;
+}
+</style>
