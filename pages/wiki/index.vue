b843785c8e2da1075401963bf64897c5e7410cc0
Recox <lucas.recoaro@gmail.com>
2021-12-17 23:38:31 +1300

    Arreglo error de links

--- pages/wiki/index.vue
+++ pages/wiki/index.vue
@@ -6,11 +6,11 @@
       <h1>Wiki / Guia de Juego</h1>
       <ul>
         <a href="https://wiki.ao20.com.ar/index.php?title=P%C3%A1gina_principal" target="_blank">Wiki General</a>
-        <li><NuxtLink to="wiki/escudos">Escudos</NuxtLink></li>
-        <li><NuxtLink to="wiki/armaduras">Armaduras</NuxtLink></li>
-        <li><NuxtLink to="wiki/armas">Armas</NuxtLink></li>
-        <li><NuxtLink to="wiki/cascos">Cascos</NuxtLink></li>
-        <li><NuxtLink to="wiki/monturas">Monturas</NuxtLink></li>
+        <li><NuxtLink to="/wiki/escudos">Escudos</NuxtLink></li>
+        <li><NuxtLink to="/wiki/armaduras">Armaduras</NuxtLink></li>
+        <li><NuxtLink to="/wiki/armas">Armas</NuxtLink></li>
+        <li><NuxtLink to="/wiki/cascos">Cascos</NuxtLink></li>
+        <li><NuxtLink to="/wiki/monturas">Monturas</NuxtLink></li>
       </ul>
     </div>
 
