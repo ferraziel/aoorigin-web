91eaa810036448878a77003ae5576ec2b8654674
Recox <lucas.recoaro@gmail.com>
2022-03-05 12:14:40 +1300

    Elimino todo lo que tiene que ver con cuentas y mercadoAO de la web

--- components/Nav.vue
+++ components/Nav.vue
@@ -21,7 +21,6 @@
             </p>
 
             <NuxtLink to="/cuenta" v-if="$auth.loggedIn">Mi cuenta</NuxtLink>
-            <NuxtLink v-else to="/login" class="px-2 py-1 text-sm border btn btn-silver">Cuenta</NuxtLink>
           </div>
         </div>
 
@@ -119,10 +118,6 @@ export default {
         {
           label: "Manual",
           route: "/wiki",
-        },
-        {
-          label: "Mercado AO",
-          route: "/mercadoao",
         }
       ],
     };
