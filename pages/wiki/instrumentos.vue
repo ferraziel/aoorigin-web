563d1441af5644596137330d45cc31ea5e11f63b
Recox <lucas.recoaro@gmail.com>
2022-03-11 12:19:39 +1300

    Arreglo instrumentos magicos

--- pages/wiki/instrumentos.vue
+++ pages/wiki/instrumentos.vue
@@ -4,7 +4,7 @@
       v-if="instrumentos.length"
       class="bg-gray-900 border-2 border-gr border-gr-primary"
     >
-      <h1>Instrumentos musicales</h1>
+      <h1>Instrumentos Musicales</h1>
       <div class="xl:col-span-10 text-xs overflow-x-auto">
         <table class="text-gray-400">
           <thead>
@@ -35,7 +35,7 @@
       v-if="instrumentosMagicos.length"
       class="bg-gray-900 border-2 border-gr border-gr-primary"
     >
-      <h1>instrumentosMagicos musicales</h1>
+      <h1>Instrumentos Musicales Magicos</h1>
       <div class="xl:col-span-10 text-xs overflow-x-auto">
         <table class="text-gray-400">
           <thead>
@@ -43,7 +43,9 @@
               <th>Id</th>
               <th></th>
               <th>Nombre</th>
-              <th>Duracion Efecto</th>
+              <th>No se Cae</th>
+              <th>Intirable</th>
+              <th>Intransferible</th>
             </tr>
           </thead>
 
@@ -52,7 +54,9 @@
               <td class="text-right">{{ instrumentosMagico.item_id }}</td>
               <td><img width="300px" :src="instrumentosMagico.Data.canvasImage" /></td>
               <td class="text-right">{{ instrumentosMagico.Data.NAME }}</td>
-              <td class="text-right">{{ instrumentosMagico.Data.DURACIONEFECTO }}</td>
+              <td class="text-right"><span v-if="instrumentosMagico.Data.NOSECAE">Sí</span><span v-else>No</span></td>
+              <td class="text-right"><span v-if="instrumentosMagico.Data.INTIRABLE">Sí</span><span v-else>No</span></td>
+              <td class="text-right"><span v-if="instrumentosMagico.Data.INSTRANSFERIBLE">Sí</span><span v-else>No</span></td>
             </tr>
           </tbody>
         </table>
