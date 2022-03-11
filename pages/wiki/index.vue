426e80cae02ef077c575ad1b0ff4c7ed91260822
Recox <lucas.recoaro@gmail.com>
2022-03-11 16:18:01 +1300

    Mejoro un poco el indice

--- pages/wiki/index.vue
+++ pages/wiki/index.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="container">
-    <br>
-    <br>
-    <div class="flex items-center text-lg md:text-xl">
+    <section>
+      <br>
+      <br>
+      <br>
       <h1>Wiki / Guia de Juego</h1>
-      <ul>
-        <a href="https://wiki.ao20.com.ar/index.php?title=P%C3%A1gina_principal" target="_blank">Wiki General (Desactualizada)</a>
-        <li><NuxtLink to="/wiki/escudos">Escudos</NuxtLink></li>
-        <li><NuxtLink to="/wiki/armaduras">Armaduras</NuxtLink></li>
-        <li><NuxtLink to="/wiki/armas">Armas</NuxtLink></li>
-        <li><NuxtLink to="/wiki/cascos">Cascos</NuxtLink></li>
-        <li><NuxtLink to="/wiki/monturas">Monturas</NuxtLink></li>
-        <li><NuxtLink to="/wiki/hechizos">Hechizos</NuxtLink></li>
-        <li><NuxtLink to="/wiki/npcs">Npcs</NuxtLink></li>
-        <li><NuxtLink to="/wiki/quests">Quests</NuxtLink></li>
-        <li><NuxtLink to="/wiki/balance">Balance</NuxtLink></li>
-        <li><NuxtLink to="/wiki/alimentos">Alimentos</NuxtLink></li>
-        <li><NuxtLink to="/wiki/resistencia-magica">Resistencia Magica</NuxtLink></li>
-        <li><NuxtLink to="/wiki/instrumentos">Instrumentos Musicales</NuxtLink></li>
-        <li><NuxtLink to="/wiki/flechas-municiones">Flechas y municiones</NuxtLink></li>
-        <li><NuxtLink to="/wiki/herramientas">Herramientas</NuxtLink></li>
-        <li><NuxtLink to="/wiki/pociones">Pociones</NuxtLink></li>
-        <li><NuxtLink to="/wiki/embarcaciones">Embarcaciones</NuxtLink></li>
-        <li><NuxtLink to="/wiki/items-magicos">Items Magicos</NuxtLink></li>
-        <li><NuxtLink to="/wiki/habilidades">Habilidades</NuxtLink></li>
-      </ul>
-    </div>
+      <h2>Esta wiki toma dinamicamente los datos desde los archivos del juego, por lo que esta siempre actualizada.</h2>
+      <div class="flex items-center text-xs md:text-xs">
+        <ul>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/escudos">Escudos</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/armaduras">Armaduras</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/armas">Armas</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/cascos">Cascos</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/monturas">Monturas</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/hechizos">Hechizos</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/npcs">Npcs</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/quests">Quests</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/balance">Balance</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/alimentos">Alimentos</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/resistencia-magica">Resistencia Magica</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/instrumentos">Instrumentos Musicales</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/flechas-municiones">Flechas y municiones</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/herramientas">Herramientas</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/pociones">Pociones</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/embarcaciones">Embarcaciones</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/items-magicos">Items Magicos</NuxtLink></li>
+          <li class="bg-yellow-500"><NuxtLink to="/wiki/habilidades">Habilidades</NuxtLink></li>
+          <li class="bg-yellow-500"><a href="https://wiki.ao20.com.ar/index.php?title=P%C3%A1gina_principal" target="_blank">Wiki Vieja (Desactualizada)</a></li>
+        </ul>
+      </div>
+    </section>
 
 
   </div>
