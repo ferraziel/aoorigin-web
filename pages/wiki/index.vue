3e852870a9aceb4bb97e368aa56a07675984cf46
Recox <lucas.recoaro@gmail.com>
2022-03-11 15:15:48 +1300

    Agrego promedios de vida y habilidades

--- pages/wiki/index.vue
+++ pages/wiki/index.vue
@@ -23,6 +23,7 @@
         <li><NuxtLink to="/wiki/pociones">Pociones</NuxtLink></li>
         <li><NuxtLink to="/wiki/embarcaciones">Embarcaciones</NuxtLink></li>
         <li><NuxtLink to="/wiki/items-magicos">Items Magicos</NuxtLink></li>
+        <li><NuxtLink to="/wiki/habilidades">Habilidades</NuxtLink></li>
       </ul>
     </div>
 
