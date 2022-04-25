1423d870c236ffa188c3af2f3f8a64f85aeaf8fa
Recox <lucas.recoaro@gmail.com>
2022-04-25 12:51:12 +1200

    Agrego seccion para items canjeables por puntos patreon

---
+++ pages/wiki/items-patreon.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div
+      v-if="patreonItems.length"
+      class="bg-gray-900 border-2 border-gr border-gr-primary"
+    >
+      <h1>Items Patreon</h1>
+      <div class="xl:col-span-10 text-xs overflow-x-auto">
+        <table class="text-gray-400">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th></th>
+              <th>Nombre</th>
+              <th>Caracteristicas</th>
+              <th>Puntos Patreon</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="patreonItems in patreonItems" :key="patreonItems.item_id">
+              <td class="text-right">{{ patreonItems.item_id }}</td>
+              <td><img width="300px" :src="patreonItems.Data.canvasImage" /></td>
+              <td class="text-right">{{ patreonItems.Data.NAME }}</td>
+              <td class="text-right">{{patreonItems.Data.TEXTO}}</td>
+              <td class="text-right">{{patreonItems.Data.VALOR}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <section v-else class="text-center mt-24">
+      <p class="text-2xl">Cargando items Patreon.</p>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      patreonItems: [],
+    };
+  },
+  async fetch() {
+    this.patreonItems = await this.$axios.$get("https://api-staging.ao20.com.ar:11812/dats/getAllPatreonPointsItems");
+  },
+  head() {
+    return {
+      title: "Wiki - Patreon Items",
+    };
+  },
+};
+</script>
+
+<style>
+
+select:required:invalid {
+  color: gray;
+}
+
+option[value=""][disabled] {
+  display: none;
+}
+
+td {
+  @apply bg-gray-900 p-4 border border-gray-200;
+}
+
+</style>
