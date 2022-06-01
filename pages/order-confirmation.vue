18e2999857408c21f0e461f1b5b8381fe2c5b82c
Recox <lucas.recoaro@gmail.com>
2022-06-02 02:44:57 +1200

    Mejoro mensajes de confirmacion

--- pages/order-confirmation.vue
+++ pages/order-confirmation.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <h1 class="mb-12 text-5xl text-primary">Confirmacion de orden</h1>
 
-    <section v-if="statusOrderAO20 == 'success'">Tu orden de compra esta confirmada, disfruta tu item.</section>
-    <section v-else-if="statusOrderAO20 == 'pending'">Tu orden esta pendiente, por favor espera unos minutos a que procese.</section>
+    <section v-if="statusOrderAO20 == 'success'">Tu orden de compra esta siendo procesada correctamente, pronto recibiras un email con instrucciones. Recorda que tu item sera depositado en la boveda</section>
+    <section v-else-if="statusOrderAO20 == 'pending'">Tu orden esta pendiente de pago, por favor espera unos momentos y verifica que el pago fue procesado.</section>
     <section v-else-if="statusOrderAO20 == 'failure'">Tu orden de compra no pudo ser completada, por favor intenta de nuevo con otro metodo de pago.</section>
     <section v-else>Hubo un error en tu orden de compra, por favor intentalo de nuevo.</section>
   </div>
