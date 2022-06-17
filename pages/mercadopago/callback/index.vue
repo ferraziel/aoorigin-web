4c3a66a412d46050a883bc96739c570b7ca05151
Recox <lucas.recoaro@gmail.com>
2022-06-17 23:50:49 +1200

    Agrego mercadopago callback page

---
+++ pages/mercadopago/callback/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  async asyncData({ redirect, $axios }) {
+    try {
+      await $axios.post(`/mercadopago/callback?code=${this.$route.query.code}`);
+      return redirect("/cuenta");
+    } catch (e) {
+      return redirect("/");
+    }
+  },
+};
+</script>
