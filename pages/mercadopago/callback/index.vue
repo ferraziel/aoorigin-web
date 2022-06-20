5ab4dc11f75b77c753c935c1603ebb2e2e7d6ab8
Recox <lucas.recoaro@gmail.com>
2022-06-20 21:08:23 +1200

    Arreglo pagina de callback de mercadopago

--- pages/mercadopago/callback/index.vue
+++ pages/mercadopago/callback/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>Seteando token de mercadopago</div>
+  <div>Seteando token de mercadopago
+    <span>{{ error }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-  async asyncData({ redirect, $axios }) {
-    await $axios.post(`mercadopago/callback?code=${this.$route.query.code}`);
-    return redirect("/cuenta");
+  async asyncData({ route, redirect, $axios }) {
+    try {
+      await $axios.get(`mercadopago/callback?code=${route.query.code}`);
+      return redirect("/cuenta");
+    } catch (error) {
+      console.error(error);
+      return {
+        error: error.message,
+      };
+    }
   },
 };
 </script>
