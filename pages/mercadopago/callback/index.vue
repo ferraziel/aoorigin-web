c26f16d0cc0724fe795bcce3fc353247ed6a0ab4
Recox <lucas.recoaro@gmail.com>
2022-07-03 17:24:09 +1200

    Agrego mejor validacion en asyncData de mercadopago callback

--- pages/mercadopago/callback/index.vue
+++ pages/mercadopago/callback/index.vue
@@ -1,20 +1,25 @@
 <template>
   <div>Seteando token de mercadopago
-    <span>{{ error }}</span>
+    <span>{{ errorDisplay }}</span>
   </div>
 </template>
 
 <script>
 export default {
+
   async asyncData({ route, redirect, $axios }) {
+    let errorDisplay = "";
+    console.log(666, route.query.code);
     try {
       await $axios.get(`mercadopago/callback?code=${route.query.code}`);
       return redirect("/cuenta");
     } catch (error) {
-      console.error(error);
-      return {
-        error: error.message,
-      };
+      console.log(error, route.query.code);
+      errorDisplay = error.message
+    }
+
+    return {
+      errorDisplay,
     }
   },
 };
