86bf6e8ccc2bc778a43e0169389cfe75f515dc02
Recox <lucas.recoaro@gmail.com>
2022-07-15 19:32:58 +1200

    Commented autoOpen mercadopago preference

--- pages/mercadoao/pagar-compra-pj/_txSignature.vue
+++ pages/mercadoao/pagar-compra-pj/_txSignature.vue
@@ -65,7 +65,7 @@ export default {
               container: ".cho-container", // Indica el nombre de la clase donde se mostrará el botón de pago
               label: "Pagar con MercadoPago", // Cambia el texto del botón de pago (opcional)
             },
-            autoOpen: true,
+            // autoOpen: true,
             theme: {
                 elementsColor: '#8da811',
                 headerColor: '#402a87'
