2261c82ca97fd91a1b4e8fb7757f777b1513cb21
AleC <anormal@gmail.com>
2022-08-12 01:00:51 -0300

    Borrado el código viejo

--- pages/wiki/monturas.vue
+++ pages/wiki/monturas.vue
@@ -59,52 +59,11 @@
               </div><!-- /Item -->
           </div><!-- /v-for -->
       </div><!-- /v-if -->
-      <!--
-    <div
-      v-if="monturas.length"
-      class="bg-gray-900 border-2 border-gr border-gr-primary"
-    >
-      <h1>Monturas</h1>
-      <div class="xl:col-span-10 text-xs overflow-x-auto">
-        <table class="text-gray-400">
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th></th>
-              <th>Nombre</th>
-              <th>Nivel Minimo</th>
-              <th>Skills Requeridos</th>
-              <th>Valor en Oro</th>
-              <th>Velocidad</th>
-              <th>No se Cae</th>
-              <th>Intirable</th>
-              <th>Intransferible</th>
-              <th>Clases Prohibidas</th>
-            </tr>
-          </thead>
 
-          <tbody>
-            <tr v-for="montura in monturas" :key="montura.item_id">
-              <td class="text-right">{{ montura.item_id }}</td>
-              <td><img width="300px" :src="montura.Data.canvasImage" /></td>
-              <td class="text-right">{{ montura.Data.NAME }}</td>
-              <td class="text-right">{{ montura.Data.MINELV }}</td>
-              <td class="text-right">{{ montura.Data.SKILLREQUERIDO }}</td>
-              <td class="text-right">{{ montura.Data.VALOR }}</td>
-              <td class="text-right">{{ montura.Data.VELOCIDAD }}</td>
-              <td class="text-right"><span v-if="montura.Data.NOSECAE">Sí</span><span v-else>No</span></td>
-              <td class="text-right"><span v-if="montura.Data.INTIRABLE">Sí</span><span v-else>No</span></td>
-              <td class="text-right"><span v-if="montura.Data.INSTRANSFERIBLE">Sí</span><span v-else>No</span></td>
-              <td class="text-right">{{montura.Data.CP1}} {{montura.Data.CP2}} {{montura.Data.CP3}} {{montura.Data.CP4}} {{montura.Data.CP5}} {{montura.Data.CP5}} {{montura.Data.CP7}} {{montura.Data.CP8}} {{montura.Data.CP9}} {{montura.Data.CP10}} {{montura.Data.CP11}} {{montura.Data.CP12}} {{montura.Data.CP13}}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
--->
     <section v-else class="text-center">
       <p class="text-2xl">Cargando monturas.</p>
     </section>
+
   </div>
 </template>
 
