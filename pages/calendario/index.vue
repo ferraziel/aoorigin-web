3b1dc6829f24a2d0046fa2cb46156f3723304c70
Recox <lucas.recoaro@gmail.com>
2022-11-03 20:53:07 +1300

    Added again the events calendar

---
+++ pages/calendario/index.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div class='app'>
+    <FullCalendar :options='calendarOptions' />
+  </div>
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+//Obtenemos informacion de los eventos en el archivo json
+import calendario from '@/assets/calendario.json';
+
+export default {
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarOptions: {
+        locale: 'es',
+        height: 650,
+        timeZone: "UTC",
+        plugins: [interactionPlugin, timeGridPlugin, dayGridPlugin],
+        initialView: 'dayGridMonth',
+        nowIndicator: true,
+        editable: true,
+        initialEvents: calendario.events
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .app {
+    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+</style>
