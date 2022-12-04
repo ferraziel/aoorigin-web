69a51e348b60b7f11add259aa7cb56e3ce81903b
Recox <lucas.recoaro@gmail.com>
2022-12-04 20:19:51 +1300

    Fixed Minehost logo image

--- components/Footer.vue
+++ components/Footer.vue
@@ -18,7 +18,7 @@
           <p class="mb-4 text-xs text-gray-600">
             <a href="https://web.minehost.com.ar/buy/index.php" target="_blank" rel="noopener">
               El servidor de Argentum 20 está hosteado en Minehost
-              <img width="200px" src="https://www.argentum20.com/wp-content/themes/ao20theme_2022/images/logo_minehost.png" class="mx-auto mt-2">
+              <img width="200px" src="@/assets/img/logo_minehost.png" class="mx-auto mt-2">
             </a>
           </p>
 
