dee9347b45e3c82caef89f88d30c5052e1ba877e
Lucas Recoaro <lucas.recoaro@gmail.com>
2022-12-29 22:03:43 +1300

    Added some more sections in the wiki

---
+++ pages/wiki/guia/leveleo-13-20.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <h1>Leveleo 13 - 20</h1>
+    <h5>Conseguir el hechizo "Fuerza". NIVEL 20</h5>
+    <p>
+      La Quest de Fuerza se consigue en Ciénaga de las Cobras (Mapa 25). Hay que hablar con el NPC Zero "Explorador de Ullathorpe" Orientación: Saliendo de Nix, 2 mapas para arriba y 2 mapas a la derecha
+    </p>
+
+    <img src="https://lh6.googleusercontent.com/hHMCH7AaIUABTtnYdfeWk8OX1Ot9WPMPfXT7f3YB9ZS_T1WKkfADBJqbUGzt5TgeAUdjzSrRH5RPr_JCPVgXFyoEYeao_GZS1xUdcYw3xOTdHKewLeOfyMZ0qHOd3vR79YyqcUo4SdBdVp7XkkCGkpQXNERQDbwUbwoVrfZgKO2pIY1PFJaS4g=w1280">
+
+    <hr>
+
+    <h5>Conseguir el hechizo "Celeridad"  NIVEL 18</h5>
+    <p>
+      La Quest de Celeridad se consigue en Bosque de Tigres (Mapa 194). Hay que hablar con el NPC Dragga "Orca Lugareña" Orientación: 2 mapas hacia arriba de Penthar
+    </p>
+
+    <img src="https://lh5.googleusercontent.com/7xlSVX3p565lK4rnld3eKUAq2Tq5s7m_6f7DDtcaaK_xgzXZJ-BmXuprx7G5i9zOJGugkCW5zIdd2NDZOk-CU7jxIlV6EZuIpUM2AM60-7ypMaWQivD-_aKtE9X5AnwyZrZkZbR_or3TXen3fQWtKc5zpBG8HT2P1hIEtDPZBzckztmyuQVirQ=w1280">
+
+    <hr>
+
+    <h5>Conseguir Petrificas (Magia de Semi guerreras) Paraliza a NPCS. NIVEL 18</h5>
+    <p>
+      Hablar con Margareth en Ullathrop y agarrar la quest "Arachnida" como recompenza nos dara el hechizo. Para llegar a hasta el dungeon. Tenemos que entrar a las catacumbas de ullathorp ubicadas abajo a la izquierda en la ciudad.
+    </p>
+    <img src="https://lh3.googleusercontent.com/Vx0j5W8rzFeBJtzgrFkURdjipwe2pXtw3w6AyDTwdHdKSWjRAT7fIete69zmiCbarnRGQpqhtngXGohXEExGKfR1PuLjXNZmcZ_YJ4DASphl3V8G317SLPVM6kmRC3oMlF4qDD0xpeD7pjFB5nudUfAOaYHb9cFKG_a4aW7uul9jHkvxJcWIwQ=w1280">
+
+    <p>
+      Una vez que entramos nos dirigimos hacia donde indica la imagen y encontraremos la entrada de las CATACUMBAS NIVEL 2. Pero ojo para entrar necesitaremos la llave para abrir la puerta, esa llave se consigue matando las ARAÑAS del mapa de la entrada. Las cuales tiene un % de drop del item
+    </p>
+
+    <img src="https://lh5.googleusercontent.com/5D95CljokYKX5gX0nuxrkElikRr7UtmdZQAg-EgxSg0YEEqyC_vzu7zO3daXWKMTHokZV8AmCiQWqZPLxecd8HngX6CNsl5GVzGmAbVpKFDQDBhKcfFnR0ZPV0hKdrFQiGDigVUuMEaYceLzVVT7hHr2aIvmJSkB187XDfeoGpEC2MgO28Fxow=w1280">
+
+    <hr>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    };
+  },
+  async fetch() {
+  },
+  head() {
+    return {
+      title: "Wiki - Leveleo 13-20"
+    };
+  },
+};
+</script>
+
+<style>
+
+select:required:invalid {
+  color: gray;
+}
+
+option[value=""][disabled] {
+  display: none;
+}
+
+</style>
---
+++ pages/wiki/insignias.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <br>
+    <br>
+    <h1>¿Cómo consigo Insignias?</h1>
+
+    <p>
+      Las insignias es uno de los items mas preciados de todo Argentum20 ya que
+      con ellas podemos conseguir armas, armaduras, cascos y hasta monturas.
+    </p>
+
+        <img src="https://lh5.googleusercontent.com/xsP5bSo-0xjOh87mrlHhkS5KBOrOZkyLlHT-M2O8Ka7f2EpdK9f1sMTTTfG09ztLpgT2uyV95hD4J-ND36MElddIXGAw7J__VhHu6sTQoJL7f4Pm5S9LS0XsRPfi8xxZ4qzZISpUGbGPyyjMQ_W8b0apAB3AEirIJ_4-2qT9AfPk7fpetcMg=w1280"/>
+    <img src="https://lh4.googleusercontent.com/Sb51LyxywC20MUbroLgJk2rNptAdlD41HfNO8pCO1n3ga1fiGSJdqOSZ5dO7mgrdBVFUgEkXsfEXHERkEsyqx2Hf24knYIwPczPeVar6pwzhJVBz5RdiFTlZabbKBYbNMFRj5N9aQBhXUABSAMH6lYMWrka5mIlObh3KlHQBSU2cdAO5WnEWdg=w1280"/>
+
+    Podemos conseguirlas de 2 maneras.
+    <ul>
+      <li>
+        En 1er lugar, matando cualquiera de los bosses que hay en las tierras de
+        Argentum20. Estos tiene un 100% de chance que se dropee una insignia,
+        Roja o Azul.
+      </li>
+
+      <li>
+        En 2do lugar, apartir de nivel 40 se nos habilitan 2 nuevos dungeons
+        donde vamos a encontrar nuevas criaturas que algunas tiene un % de
+        dropeo de insignias
+      </li>
+
+      <li>
+        Dungeon Magma: Golem Infernal (Insignia roja), Banshee (Insignia azul).
+      </li>
+
+      <li>Cueva de la Gorgona: Euríale y Dama Oscura (Ambas Insignia Roja).</li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  async fetch() {},
+  head() {
+    return {
+      title: "Wiki - Insignias",
+    };
+  },
+};
+</script>
+
+<style>
+select:required:invalid {
+  color: gray;
+}
+
+option[value=""][disabled] {
+  display: none;
+}
+</style>
