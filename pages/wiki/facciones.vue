1dcf71f179fc78da197e64dc950e068a12efca79
Lucas Recoaro <lucas.recoaro@gmail.com>
2022-12-30 10:47:19 +1300

    Fixed images leveleo 1-13 section

---
+++ pages/wiki/facciones.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <br>
+    <br>
+    <h2 class="section-title text-center mb-4 uppercase">Facciones</h2>
+    <h3>Armada Real</h3>
+    <img src="@/assets/img/wiki/armada-real.png"/>
+
+    <p>
+      Para enlistarnos en la Armada real necesitamos tener al menos 100 criminales matados y ser nivel 18 para el primer rango, sin tener ningún ciudadano matado o haber pedido el perdón faccionario. Nos dirigimos al Rey de Banderbille que se encuentra en el Castillo en el mapa 60. Lo clickeamos y tipeamos /ENLISTAR
+    </p>
+
+    <ul>
+      <li>1Rango= 100 criminales matados -Soldado Nivel 18</li>
+      <li>2Rango= 350 criminales matados -Caballero Nivel 30</li>
+      <li>3Rango= 700 criminales matados  -Capitán Nivel 35</li>
+      <li>4Rango= 1050 criminales matados -Protector del Reino Nivel 40</li>
+      <li>5Rango= 2000 criminales matados  -Campeón de la Luz Nivel 43</li>
+    </ul>
+
+    <h6>Por cada rango que subamos recibiremos una mejor armadura faccionaria.</h6>
+
+    <br>
+    <hr>
+    <br>
+
+    <h3>Legion del Caos</h3>
+    <img src="@/assets/img/wiki/legion-caos.png"/>
+
+    <p>
+      Para enlistarnos en la Armada del Caos necesitamos tener al menos 100 ciudadanos matadados y ser nivel 25 para el primer rango, no haber pertenecido a la faccion contraria o tener el perdon del concilio. Nos dirigimos hacia el Dictador de Arkhein que se encuentra en el palacio en el mapa 295. Lo clickeamos y tipeamos /ENLISTAR
+    </p>
+
+    <ul>
+      <li>1Rango=100 cidadanos matados -Acólito Nivel 25</li>
+      <li>2Rango=350 cidadanos matados -Emisario del Caos Nivel 30</li>
+      <li>3Rango=700 cidadanos matados -Sanguinario Nivel 35</li>
+      <li>4Rango=1050 cidadanos matados -Caballero de la Oscuridad Nivel 40</li>
+      <li>5Rango=2000 cidadanos matados - Devorador de Almas Nivel 43</li>
+    </ul>
+
+    <h6>Por cada rango que subamos recibiremos una mejor armadura faccionaria.</h6>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  async fetch() {},
+  head() {
+    return {
+      title: "Wiki - Facciones",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- pages/wiki/guia/leveleo-1-12.vue
+++ pages/wiki/guia/leveleo-1-12.vue
@@ -14,9 +14,9 @@
       Encontraras un NPC con un signo de exclamacion en la cabeza. Apreta doble click sobre el para interactual y que te brinde la informacion de las quest.
     </p>
 
-    <img src="https://lh4.googleusercontent.com/8CnSg-xhs498yjQLa7l1cbtIeR7TCacVnN1mJaUdkeoN6JR7t9INgbIt_paArTrJUmBBlrQarPSF63W-G4wv9TN4JgIGqcWjWD4DKSspG-8GeQwnYChsJJJOQ_dXq4oQWyTHJdUxCCVeijzm-e19vCt2IuS6c4JggH_qt655BP0HGq8EaItWwQ=w1280">
-    <img src="https://lh6.googleusercontent.com/N0PENN20LdoePGT6WQ7Rrs643IUETFCWQWT112Ea2MrBVkSGzjSDSdSa0jYmh7eY49p4tuPHgKJl5_2dX_gghfhcNGp5TsRaxowJrOG4KFkQweG8r7nzIbXIX4EhOJO1ixUVx3ZtDfkS1PtD-1gUorUzdBVsdRO-v9RkutL6Pf5SWgt2M3jUYQ=w1280">
-    <img src="https://lh4.googleusercontent.com/clHtz9LpdaTLIJbmj411EcO6XzBuBLLxWQmXD18qE1eMBgn0_bGVIiuIfmTobHZQoHI2vYTHCAT-ACBv_wUYBDL4aT7AY5XY9z2TRHDm_yFvZBZ2tZ25akr46vaGcIcT8EdQJdlpAfzaCBITHCAtJiDp9UjmOgasE4fTL5f9opydwrb0_K4tMw=w1280">
+    <img src="@/assets/img/wiki/leveleo1-13-a.png"/>
+    <img src="@/assets/img/wiki/leveleo1-13-b.png"/>
+    <img src="@/assets/img/wiki/leveleo1-13-c.png"/>
 
     <p>
       Mientras subimos de nivel matando LOBOS, SERPIENTES, MURCIELAGOS y ESCORPIONES. Podremos cumplir las misiones de este NPC hasta llegar al NIVEL 12 que esta la mision mas importante de todas. Donde nos dan de recomenza varios items que nos brindan una gran ayuda despues de dejar de ser NEWBIES. NO OLVIDEN esta quest porque es muy importante de hacer en nivel bajo.
