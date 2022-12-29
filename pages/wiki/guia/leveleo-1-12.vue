91a2e00999ec33513a06243bc694921f2bbb826e
Lucas Recoaro <lucas.recoaro@gmail.com>
2022-12-30 11:07:25 +1300

    Improved and finished training guide for level 13-20

--- pages/wiki/guia/leveleo-1-12.vue
+++ pages/wiki/guia/leveleo-1-12.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
-
-    <h1>Leveleo 1 - 12</h1>
+    <h2 class="section-title text-center mb-4 uppercase">Leveleo 1 - 12</h2>
     <p>
       Bienvenido al mundo de Argentum20 y Bienvenido al Newbie Dungeon. Este va ser tu primer lugar de entrenamiento, el primer dungeon que vas a visitar. Aqui encontraras criatura de bajo nivel, pero no te confies que recien empiezas y puede costar en un principio controlar y derrotar las criaturas.
     </p>
@@ -14,14 +13,13 @@
       Encontraras un NPC con un signo de exclamacion en la cabeza. Apreta doble click sobre el para interactual y que te brinde la informacion de las quest.
     </p>
 
-    <img src="@/assets/img/wiki/leveleo1-13-a.png"/>
-    <img src="@/assets/img/wiki/leveleo1-13-b.png"/>
-    <img src="@/assets/img/wiki/leveleo1-13-c.png"/>
+    <img src="@/assets/img/wiki/leveleo1-12-a.png"/>
+    <img src="@/assets/img/wiki/leveleo1-12-b.png"/>
+    <img src="@/assets/img/wiki/leveleo1-12-c.png"/>
 
     <p>
       Mientras subimos de nivel matando LOBOS, SERPIENTES, MURCIELAGOS y ESCORPIONES. Podremos cumplir las misiones de este NPC hasta llegar al NIVEL 12 que esta la mision mas importante de todas. Donde nos dan de recomenza varios items que nos brindan una gran ayuda despues de dejar de ser NEWBIES. NO OLVIDEN esta quest porque es muy importante de hacer en nivel bajo.
     </p>
-
   </div>
 </template>
 
--- pages/wiki/guia/leveleo-13-20.vue
+++ pages/wiki/guia/leveleo-13-20.vue
@@ -1,12 +1,61 @@
 <template>
   <div class="container">
-    <h1>Leveleo 13 - 20</h1>
+    <h2 class="section-title text-center mb-4 uppercase">Leveleo 13 - 20</h2>
+
+    <p>
+      Llegamos a nivel 13. Dejamos de ser newbies ahora no disponemos mas de las pociones que nos dieron al inicio. Por esto es muy importante que recuerden hacer la quest del newbie dungeon antes de pasar a nivel 13. Ya que esta quest de da pociones para continuar tu aventura.
+    </p>
+
+    <p>
+      A partir de lvl 13 empezaremos a recorrer el mundo guiandonos por en mapa interactivo del juego viendo todos los mapas. Voy a marcar algunos mapas y algunas quest importantes para avanzar en esta etapa.
+    </p>
+
+    <p>
+      En tu primera aventura despues de salir del newbie dungeon encontraras un continente enorme por recorrer lleno de diversas criaturas que tendras que derrotar para superar los siguientes niveles.
+      <img src="@/assets/img/wiki/leveleo13-20-e.png"/>
+      En el siguiente Mapa estan marcadas todas las CIUDADES y zonas SEGURAS del continente que recorreras. Cerca de estas ciudades comenzaremos nuestra travesía con las diferentes criaturas que encontremos.
+    </p>
+
+    <hr>
+    <h2>Lugares para entrenar</h2>
+
+    <p>Para comenzar podriamos ir los siguientes mapas marcados, son mapas que encontraremos plagado de Lobos. Una criatura que rinde por su buena exp en early, por su poca vida y su gran cantidad en estos mapas.
+      <img src="@/assets/img/wiki/leveleo13-20-f.png"/>
+    </p>
+
+    <p style="color:red">Mapa 158 (Abajo de bander), Mapa 8 (A la izquierda de Ullathorp) y Mapa 24 (Arriba de Arkhein). En estos mapas podras ganar buena experiencia para tus primeros niveles despues del 13. Lobo 90 de vida, 130 de exp, Ataque 10/23</p>
+    <p>CEMENTERIO DE NIX, en el mapa 4 arriba de la ciudad de NIX podremos encontrar el mapa de cementerio con su dungeon para niveles hasta 20. Es un gran mapa para ir luego de salir del newbie dungeon. Son npc con poco daño y buena experiencia, aparte de que dispone de algunas quest este mapa/dungeon.</p>
+    <p style="color:blue">Tambien puedes encontrar criaturas un poco mas poderosas por estas zonas marcadas con color azul en el mapa. Aqui podremos encontrar criaturas que rinden pero su golpe es mas fuerte de que por ahora estamos acostumbrados. Las criaturas son "Tortugas Mediterraneas"  500 de vida 600 de experiencia y de recompenza un escudo de tortura. Estan en los mapas 75,  74, 73, 72, 71</p>
+    <p style="color:yellow">Por ultimo tenemos varios mapas donde podremos encontrar Osos pardos, donde tambien conseguiremos la quest de "Fuerza" apartir de lvl 20. Oso Pardo 600 de vida, 850 de experiencia y una piel de oso de recompenza</p>
+
+    <h2>Dungeons</h2>
+    <h4>Cementerio</h4>
+    <img src="@/assets/img/wiki/leveleo13-20-g.png"/>
+    <p>Ubicado al Norte de Nix es el mapa y dungeon predilecto para los niveles mas bajos. Luego de salir del dungeon newbie.</p>
+
+    <img src="@/assets/img/wiki/leveleo13-20-h.png"/>
+    <p>Aquí puedes encontrar diversos tipos de npc hostiles los cuales son acordes a los niveles mas bajos. También encontraras algunas quest para realizar en el mapa.</p>
+
+
+    <h4>Mausoleo (hasta nivel 20)</h4>
+    <p>Este va ser el primer dungeon donde entrenemos luego dungeon newbie. Aqui podemos estar hasta lvl 20 y no nos encontraremos con gente nivel alta.</p>
+    <img src="@/assets/img/wiki/leveleo13-20-i.png"/>
+
+    <p>Es uno de los unicos dungeon con limite de nivel de todo el mundo de Argentum20</p>
+    <img src="@/assets/img/wiki/leveleo13-20-j.png"/>
+    <img src="@/assets/img/wiki/leveleo13-20-k.png"/>
+    <img src="@/assets/img/wiki/leveleo13-20-l.png"/>
+
+    <hr>
+
+    <h2>Quest Hechizos</h2>
+
     <h5>Conseguir el hechizo "Fuerza". NIVEL 20</h5>
     <p>
       La Quest de Fuerza se consigue en Ciénaga de las Cobras (Mapa 25). Hay que hablar con el NPC Zero "Explorador de Ullathorpe" Orientación: Saliendo de Nix, 2 mapas para arriba y 2 mapas a la derecha
     </p>
 
-    <img src="https://lh6.googleusercontent.com/hHMCH7AaIUABTtnYdfeWk8OX1Ot9WPMPfXT7f3YB9ZS_T1WKkfADBJqbUGzt5TgeAUdjzSrRH5RPr_JCPVgXFyoEYeao_GZS1xUdcYw3xOTdHKewLeOfyMZ0qHOd3vR79YyqcUo4SdBdVp7XkkCGkpQXNERQDbwUbwoVrfZgKO2pIY1PFJaS4g=w1280">
+    <img src="@/assets/img/wiki/leveleo13-20-a.png"/>
 
     <hr>
 
@@ -15,7 +64,7 @@
       La Quest de Celeridad se consigue en Bosque de Tigres (Mapa 194). Hay que hablar con el NPC Dragga "Orca Lugareña" Orientación: 2 mapas hacia arriba de Penthar
     </p>
 
-    <img src="https://lh5.googleusercontent.com/7xlSVX3p565lK4rnld3eKUAq2Tq5s7m_6f7DDtcaaK_xgzXZJ-BmXuprx7G5i9zOJGugkCW5zIdd2NDZOk-CU7jxIlV6EZuIpUM2AM60-7ypMaWQivD-_aKtE9X5AnwyZrZkZbR_or3TXen3fQWtKc5zpBG8HT2P1hIEtDPZBzckztmyuQVirQ=w1280">
+    <img src="@/assets/img/wiki/leveleo13-20-b.png"/>
 
     <hr>
 
@@ -23,14 +72,13 @@
     <p>
       Hablar con Margareth en Ullathrop y agarrar la quest "Arachnida" como recompenza nos dara el hechizo. Para llegar a hasta el dungeon. Tenemos que entrar a las catacumbas de ullathorp ubicadas abajo a la izquierda en la ciudad.
     </p>
-    <img src="https://lh3.googleusercontent.com/Vx0j5W8rzFeBJtzgrFkURdjipwe2pXtw3w6AyDTwdHdKSWjRAT7fIete69zmiCbarnRGQpqhtngXGohXEExGKfR1PuLjXNZmcZ_YJ4DASphl3V8G317SLPVM6kmRC3oMlF4qDD0xpeD7pjFB5nudUfAOaYHb9cFKG_a4aW7uul9jHkvxJcWIwQ=w1280">
+    <img src="@/assets/img/wiki/leveleo13-20-c.png"/>
 
     <p>
       Una vez que entramos nos dirigimos hacia donde indica la imagen y encontraremos la entrada de las CATACUMBAS NIVEL 2. Pero ojo para entrar necesitaremos la llave para abrir la puerta, esa llave se consigue matando las ARAÑAS del mapa de la entrada. Las cuales tiene un % de drop del item
     </p>
 
-    <img src="https://lh5.googleusercontent.com/5D95CljokYKX5gX0nuxrkElikRr7UtmdZQAg-EgxSg0YEEqyC_vzu7zO3daXWKMTHokZV8AmCiQWqZPLxecd8HngX6CNsl5GVzGmAbVpKFDQDBhKcfFnR0ZPV0hKdrFQiGDigVUuMEaYceLzVVT7hHr2aIvmJSkB187XDfeoGpEC2MgO28Fxow=w1280">
-
+    <img src="@/assets/img/wiki/leveleo13-20-d.png"/>
     <hr>
 
   </div>
