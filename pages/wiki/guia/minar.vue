775a9160ab7da0e353def7f9bde8b4102c4b439b
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-01-05 11:20:47 +1300

    Added mineria guide

---
+++ pages/wiki/guia/minar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <br>
+    <br>
+    <h2 class="section-title text-center mb-4 uppercase">¿Cómo y dónde minar?</h2>
+
+    <iframe width="800" height="600" src="https://www.youtube-nocookie.com/embed/AXjkVi4E9jg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</template>
+
+<script>
+
+export default {
+  head() {
+    return {
+      title: "Wiki - Tips"
+    };
+  },
+};
+</script>
+
+<style>
+</style>
