17e0eb45aed4564889de4702b2719e7b4be4d130
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-01-11 18:07:19 +1300

    Improved message in confirmation transaction

--- pages/order-confirmation.vue
+++ pages/order-confirmation.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1 class="mb-12 text-5xl text-primary">Confirmacion de orden</h1>
 
-    <section v-if="statusOrderAO20 == 'success'">Tu orden de compra esta siendo procesada correctamente, pronto recibiras un email con instrucciones. Recorda que tu item sera depositado en la boveda</section>
+    <section v-if="statusOrderAO20 == 'success'">Tu orden de compra esta siendo procesada correctamente, pronto recibiras un email con instrucciones. Si compraste un item sera depositado en la boveda, Si compraste un personaje asegurate de tener un slot de personaje disponible.</section>
     <section v-else-if="statusOrderAO20 == 'pending'">Tu orden esta pendiente de pago, por favor espera unos momentos y verifica que el pago fue procesado.</section>
     <section v-else-if="statusOrderAO20 == 'failure'">Tu orden de compra no pudo ser completada, por favor intenta de nuevo con otro metodo de pago.</section>
     <section v-else>Hubo un error en tu orden de compra, por favor intentalo de nuevo.</section>
