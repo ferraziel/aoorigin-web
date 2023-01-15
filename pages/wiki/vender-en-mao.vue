b0a7a697a6537446be84d6e030e51693c98d4b1b
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-01-15 14:23:47 +1300

    Added wiki for mao

---
+++ pages/wiki/vender-en-mao.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <br>
+    <br>
+    <h2 class="section-title text-center mb-4 uppercase">¿Cómo publico mi personaje en MAO?</h2>
+    <img src="@/assets/img/wiki/mao-a.png"/>
+    <p>
+      Encontraras un NPC en el Meson Hostigado (Mapa 170, tiene entrada por Banderbill y Arkhein). El mismo se llama Vendedor de Personajes, interactua con el para publicarlo.
+    </p>
+    <img src="@/assets/img/wiki/mao-b.png"/>
+
+    <span>Luego tendras que esperar unos minutos hasta que aparezca el personaje en la web.</span>
+    <br>
+    <span>Recuerda linkear tu cuenta de MercadoPago con la cuenta de AO20 para poder recibir el pago. Esta operacion se realiza desde Mi Cuenta en la web</span>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  head() {
+    return {
+      title: "Wiki - Vender en MAO"
+    };
+  },
+};
+</script>
+
+<style>
+</style>
