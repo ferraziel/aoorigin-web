7d0eb9c5b4e8f47eb2b8718dbf66d1b7402a0a1f
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-01-18 18:36:01 +1300

    Listado de personajes e items ahora es responsive, funciona en mobile

--- pages/mercadoao/index.vue
+++ pages/mercadoao/index.vue
@@ -7,7 +7,7 @@
       <h3>Lista de items a la venta.</h3>
 
       <div v-if="itemsOnSale.length">
-        <div class="container grid grid-cols-4">
+        <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           <div class="flex flex-col items-center gap-y-4" v-for="item in itemsOnSale" :key="item.item_id" :id="item.item_id">
 
             <NuxtLink :to="`/mercadoao/item/${item.item_id}`">
@@ -16,14 +16,15 @@
               </div>
 
               <ul>
-                <li style="color: yellow">{{ item.Data.NAME }}</li>
-                <li class="text-xs">{{ item.Data.TEXTO }}</li>
-                <li style="color: green">Precio: {{ item.price_in_pesos.toLocaleString('es-AR', { style: 'currency', currency: 'ARS', minimumFractionDigits: 0, maximumFractionDigits: 0 }) }} ARS</li>
+                <h4 class="text-gr gr-gold text-lg md:text-xl lg:text-2xl">{{ item.Data.NAME }}</h4>
+                <li class="text-xs md:text-sm lg:text-base">{{ item.Data.NAME == item.Data.TEXTO ? "" : item.Data.TEXTO }}</li>
+                <li class="text-green text-sm md:text-base lg:text-lg">Precio: {{ item.price_in_pesos.toLocaleString('es-AR', { style: 'currency', currency: 'ARS', minimumFractionDigits: 0, maximumFractionDigits: 0 }) }} ARS</li>
               </ul>
             </NuxtLink>
           </div>
         </div>
       </div>
+
       <section v-else class="text-center mt-24">
         <p class="text-2xl">No hay items en venta.</p>
       </section>
@@ -32,24 +33,23 @@
 
       <h3>Lista de personajes en venta.</h3>
       <h4>Se actualiza cada 1 hs con la informacion mas reciente del mundo.</h4>
-
       <div v-if="usersOnSale.length">
-        <div class="container grid grid-cols-4">
+        <span>Hay <strong>{{usersOnSale.length}}</strong> personajes listados.</span>
+
+        <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
           <div class="flex flex-col items-center gap-y-4"
-               v-for="user in usersOnSale" :key="user.id" :id="user.id">
+              v-for="user in usersOnSale" :key="user.id" :id="user.id">
 
             <NuxtLink :to="`/mercadoao/personaje/${user.id}`">
               <div class="flex items-center justify-center border-2 border-gr border-gr-primary p-1 bg-gray-900">
-                <img :src="user.canvasImage" width="64px" />
+                <img :src="user.canvasImage" width="64px"/>
               </div>
-
               <ul>
-                <h4 class="text-gr gr-gold">{{ user.name }}</h4>
-                <li class="text-m gr-gold">Nivel: {{ user.level }}</li>
-                <li style="text-m gr-gold">{{ user.price_in_mao.toLocaleString('es-AR', { style: 'currency', currency: 'ARS', minimumFractionDigits: 0, maximumFractionDigits: 0 }) }} ARS</li>
+                <h4 class="text-gr gr-gold text-lg md:text-xl lg:text-2xl">{{ user.name }}</h4>
+                <li class="text-m gr-gold text-sm md:text-base lg:text-lg">Nivel: {{ user.level }}</li>
+                <li class="text-m gr-gold text-sm md:text-base lg:text-lg">{{ user.price_in_mao.toLocaleString('es-AR', { style: 'currency', currency: 'ARS', minimumFractionDigits: 0, maximumFractionDigits: 0 }) }} ARS</li>
               </ul>
             </NuxtLink>
-            <br>
           </div>
         </div>
       </div>
