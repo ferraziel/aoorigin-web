818633b0fed6f68a01b968de7d3c43443e27768b
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-01-18 23:26:32 +1300

    Banned list now is responsive

--- pages/baneados/index.vue
+++ pages/baneados/index.vue
@@ -8,15 +8,14 @@
       <h6>Se actualiza cada 1 hs con la informacion mas reciente del mundo.</h6>
 
       <div v-if="bannedUsers.length">
-        <div class="container grid grid-cols-4">
+        <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
           <div class="flex flex-col items-center gap-y-4"
-               v-for="user in bannedUsers" :key="user.id" :id="user.id">
+              v-for="user in bannedUsers" :key="user.name" :id="user.name">
 
-            <!-- <NuxtLink :to="`/baneados/${user.id}`"> -->
+            <!-- <NuxtLink :to="`/mercadoao/personaje/${user.name}`"> -->
             <div class="flex items-center justify-center border-2 border-gr border-gr-primary p-1 bg-gray-900">
-              <img :src="user.canvasImage" width="64px" />
+              <img :src="user.canvasImage" width="64px"/>
             </div>
-
             <ul>
               <h4 class="text-gr gr-gold">{{ user.name }}</h4>
               <li class="text-m gr-gold">Nivel: {{ user.level }}</li>
@@ -25,7 +24,6 @@
               <li class="text-xs gr-gold">Ultimo login: {{ $dayjs(user.fecha_ingreso).format("DD [de] MMMM [de] YYYY") }}</li>
             </ul>
             <!-- </NuxtLink> -->
-            <br>
           </div>
         </div>
       </div>
