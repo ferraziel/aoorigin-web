86c34b3e6a841532424c863f356df1ad1aead529
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-01-20 18:45:43 +1300

    Added level guide from lvl 25 to 40

---
+++ pages/wiki/guia/leveleo-25-40.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <br />
+    <br />
+    <h2 class="section-title text-center mb-4 uppercase">Leveleo 25 - 40</h2>
+    <h3>Dungeon Veril</h3>
+
+    <span>Dungeon Veril  25-40 Llegamos al nivel 25, es hora de explorar uno de los dungeons mas famosos de argentum, Cuenta con 8 Mapas donde podemos encontrar varias criaturas que te brindarían experiencia, medusas golems, brujas, y sobre todo pueden minar oro, para crear sus objetos de herrería.</span>
+    <img src="@/assets/img/wiki/leveleo25-40-a.png" />
+    <hr>
+    <h3>Nueva Esperanza</h3>
+
+    <span>Nueva Esperanza 25-40 donde podrás encontrar una gran cantidad de Criaturas, Liches, brujas, leviatanes, Magos malvados, orcos brujos, ogros, ogros guerreros, hadas, mágicas, lord orco pero ten cuidado, ya que es una zona totalmente insegura.</span>
+    <img src="@/assets/img/wiki/leveleo25-40-b.png" />
+    <hr>
+
+    <span> continuara ...</span>
+
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Wiki - Leveleo 18-30",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
