990c0359c922e9db05ca1423076be5fe37a2ae47
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-01-26 15:22:44 +1300

    Finished guide from level 25-40

--- pages/wiki/guia/leveleo-25-40.vue
+++ pages/wiki/guia/leveleo-25-40.vue
@@ -14,7 +14,21 @@
     <img src="@/assets/img/wiki/leveleo25-40-b.png" />
     <hr>
 
-    <span> continuara ...</span>
+    <h3>Caverna Helada.</h3>
+    <span>Para aquellos, que necesiten hacer oro de manera un poco mas rápida, Este dungeon es el correcto, se encuentra al norte del mundo en la ciudad de Eleusis Mapa 354 donde también podremos encontrar gran variedad de hechizos, Piles Para crear objetos en Sastrería y Criaturas Domables</span>
+    <span>Este Gran mundo helado cuenta con varios mapas, en uno de ellos se encuentra su cueva secreta, donde podremos encontrar al famoso yeti y a Eztbron VientoGélido el cual tiene un 100% de Drop para conseguir la Gema tan preciada para fundar clan. y insignias rojas y azules, tambien nos brindara 100 puntos de clan, pero tengo cuidado ya que es muy poderoso, es inmune al paralizar e inmovilizar</span>
+    <img src="@/assets/img/wiki/leveleo25-40-c.png" />
+    <hr>
+
+    <h3>Entrenamiento Mar Abierto.</h3>
+    <span>En estos Mapas podemos encontrar distintas criaturas que te brindaran gran cantidad de experiencia y buen oro para aquellos que necesiten entrenar, de manera un poco mas tranquila Mapa 412 Mar de Tiburones, Mapa 258 Mar de Leviantanes</span>
+    <img src="@/assets/img/wiki/leveleo25-40-d.png" />
+    <hr>
+
+    <h3>Cueva de las Nagas.</h3>
+    <span>Este dungeon, cuanta con 2 entradas que podemos encontrar variedad de Npcs para poder entrenar, en este dungeon, esta prohibido el uso de invisibilidad, y tienen que usar 2 trajes especiales, Traje de Baño, y Traje de caucho se consiguen mediante quest</span>
+    <img src="@/assets/img/wiki/leveleo25-40-e.png" />
+    <hr>
 
   </div>
 </template>
