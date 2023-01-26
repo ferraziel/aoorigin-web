3b870aa108dd0b9322bcbd938a51c05c0da949d3
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-01-26 16:00:59 +1300

    Added guia complementaria de pesca

--- pages/wiki/pescar.vue
+++ pages/wiki/pescar.vue
@@ -5,6 +5,8 @@
     <h2 class="section-title text-center mb-4 uppercase">Pescar</h2>
     <span>A continuacion veran una planilla con la lista de bonificacion de pesca de cada caña y en cada zona. Aportado por el usuario "ORU"</span>
     <img src="@/assets/img/wiki/pescar.png"/>
+    <br>
+    <a href="https://docs.google.com/spreadsheets/d/1gXrOtyPB0qarWm1_iOm4nhSeWDFUl-CsHj1nV9u0fxU/edit#gid=0">Guia complementaria de pesca</a>
   </div>
 </template>
 
