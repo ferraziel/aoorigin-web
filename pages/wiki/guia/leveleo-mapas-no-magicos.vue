867a222d1d93e36594c32a3351275c2706795901
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-01-26 19:16:19 +1300

    Fixed link leveleo mapas no magicos

---
+++ pages/wiki/guia/leveleo-mapas-no-magicos.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <br />
+    <br />
+    <h2 class="section-title text-center mb-4 uppercase">Leveleo 25 - 40</h2>
+    <h3>Montaña Enana</h3>
+    <img src="@/assets/img/wiki/leveleo-no-magicos-a.png" />
+    <img src="@/assets/img/wiki/leveleo-no-magicos-b.png" />
+    <img src="@/assets/img/wiki/leveleo-no-magicos-c.png" />
+    <hr>
+
+    <h3>Ruinas de Earost</h3>
+    <img src="@/assets/img/wiki/leveleo-no-magicos-d.png" />
+    <img src="@/assets/img/wiki/leveleo-no-magicos-e.png" />
+    <img src="@/assets/img/wiki/leveleo-no-magicos-f.png" />
+
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Wiki - Leveleo 18-30",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
