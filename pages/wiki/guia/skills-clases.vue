f23d36abe0f4a98355fc518c6f67f887a8f10e0e
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-02-01 15:38:57 +1300

    Added link to complementary guide for points and classes

--- pages/wiki/guia/skills-clases.vue
+++ pages/wiki/guia/skills-clases.vue
@@ -3,6 +3,9 @@
     <br>
     <br>
     <h2 class="section-title text-center mb-4 uppercase">¿Como Asignar Skills por Clases?</h2>
+
+    <a target="_blank" href="https://docs.google.com/spreadsheets/d/14AvEE3UhLOloJYbeKR6S1JJmNlY4ZHjOprHOjP4rAcc/edit#gid=931901389">Guia complementaria de puntos y clases</a>
+
     <span>Guia importantisima de asignacion de skills por cada clase de las tierras de Argentum20. Contenido compartido por el usuario "GORE". Gracias por tu aporte!</span>
     <img src="@/assets/img/wiki/skills-clases-a.png"/>
     <img src="@/assets/img/wiki/skills-clases-b.png"/>
