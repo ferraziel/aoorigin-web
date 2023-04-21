5f758c0f8afbfc888ad2a85dbec4e2bbe0680fe0
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-04-21 14:05:34 +1200

    Arreglo video tutorial de comenzar

--- pages/wiki/guia/comenzar.vue
+++ pages/wiki/guia/comenzar.vue
@@ -4,7 +4,7 @@
     <br>
     <h2 class="section-title text-center mb-4 uppercase">¿Como Comenzar en AO20?</h2>
 
-    <iframe width="800" height="600" src="https://www.youtube-nocookie.com/embed/AXjkVi4E9jg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="800" height="600" src="https://www.youtube.com/embed/DLHsBmMNrfc" title="AO20 Comenzar" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>
 </template>
 
