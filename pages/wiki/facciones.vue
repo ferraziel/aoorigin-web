b0e48b8e6294751765639ed1587990c55ca536ac
pLuS <plusin@gmail.com>
2023-06-21 18:16:01 -0300

    Update facciones.vue
    Changed some values

--- pages/wiki/facciones.vue
+++ pages/wiki/facciones.vue
@@ -7,11 +7,11 @@
     <img src="@/assets/img/wiki/armada-real.png"/>
 
     <p>
-      Para enlistarnos en la Armada real necesitamos tener al menos 25 criminales matados y ser nivel 21 para el primer rango, sin tener ningún ciudadano matado o haber pedido el perdón faccionario. Nos dirigimos al Rey de Banderbill que se encuentra en el Castillo en el mapa 60. Lo clickeamos y tipeamos /ENLISTAR
+      Para enlistarnos en la Armada real necesitamos tener al menos 25 criminales matados y ser nivel 25 para el primer rango, sin tener ningún ciudadano matado o haber pedido el perdón faccionario. Nos dirigimos al Rey de Banderbill que se encuentra en el Castillo en el mapa 60. Lo clickeamos y tipeamos /ENLISTAR
     </p>
 
     <ul>
-      <li>1er Rango: 25 criminales matados - Soldado (Nivel 21)</li>
+      <li>1er Rango: 25 criminales matados - Soldado (Nivel 25)</li>
       <li>2do Rango: 350 criminales matados - Caballero (Nivel 30)</li>
       <li>3er Rango: 700 criminales matados - Capitán (Nivel 35)</li>
       <li>4to Rango: 1050 criminales matados - Protector del Reino (Nivel 40)</li>
@@ -28,11 +28,11 @@
     <img src="@/assets/img/wiki/legion-caos.png"/>
 
     <p>
-      Para enlistarnos en la Armada del Caos necesitamos tener al menos 25 ciudadanos matadados y ser nivel 21 para el primer rango, no haber pertenecido a la facción contraria o tener el perdón del concilio. Nos dirigimos hacia el Dictador de Arkhein que se encuentra en el palacio en el mapa 295. Lo clickeamos y tipeamos /ENLISTAR
+      Para enlistarnos en la Armada del Caos necesitamos tener al menos 25 ciudadanos matadados y ser nivel 25 para el primer rango, no haber pertenecido a la facción contraria o tener el perdón del concilio. Nos dirigimos hacia el Dictador de Arkhein que se encuentra en el palacio en el mapa 295. Lo clickeamos y tipeamos /ENLISTAR
     </p>
 
     <ul>
-      <li>1er Rango:100 ciudadanos matados - Acólito (Nivel 21)</li>
+      <li>1er Rango: 25 ciudadanos matados - Acólito (Nivel 25)</li>
       <li>2do Rango: 350 ciudadanos matados - Emisario del Caos (Nivel 30)</li>
       <li>3er Rango: 700 ciudadanos matados - Sanguinario (Nivel 35)</li>
       <li>4to Rango: 1050 ciudadanos matados - Caballero de la Oscuridad (Nivel 40)</li>
