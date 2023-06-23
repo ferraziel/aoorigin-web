203a66afd9f45cf3ef47f20fbbeb8b541bc07aef
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-06-23 12:35:43 +1200

    Added hideHeaderAndFooter functionality

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="base" class="min-h-screen">
-    <Nav />
+    <Nav v-if="!$route.query.hideHeaderAndFooter || $route.query.hideHeaderAndFooter !== 'true'" />
 
     <div>
       <Nuxt class="pt-24" />
     </div>
 
-    <Footer />
+    <Footer v-if="!$route.query.hideHeaderAndFooter || $route.query.hideHeaderAndFooter !== 'true'" />
   </div>
 </template>
+
 
 <script>
 export default {};
