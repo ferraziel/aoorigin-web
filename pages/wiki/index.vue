b97db4fea31e8046413c31b75817485241b10452
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-06-23 12:51:42 +1200

    Links in the wiki respect the query GET params to hide/show footer/header

--- pages/wiki/index.vue
+++ pages/wiki/index.vue
@@ -8,71 +8,202 @@
 
       <!-- Items container -->
       <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full mb-6 content-rte text-xl gap-4">
-          <div><NuxtLink to="/wiki/escudos" class="btn-wiki-cat btn bg-brown-texture btn-silver">Escudos</NuxtLink></div>
-          <div><NuxtLink to="/wiki/armaduras" class="btn-wiki-cat btn bg-brown-texture btn-silver">Armaduras</NuxtLink></div>
-          <div><NuxtLink to="/wiki/armas" class="btn-wiki-cat btn bg-brown-texture btn-silver">Armas</NuxtLink></div>
-          <div><NuxtLink to="/wiki/cascos" class="btn-wiki-cat btn bg-brown-texture btn-silver">Cascos</NuxtLink></div>
-          <div><NuxtLink to="/wiki/monturas" class="btn-wiki-cat btn bg-brown-texture btn-silver">Monturas</NuxtLink></div>
-          <div><NuxtLink to="/wiki/hechizos" class="btn-wiki-cat btn bg-brown-texture btn-silver">Hechizos</NuxtLink></div>
-          <div><NuxtLink to="/wiki/npcs" class="btn-wiki-cat btn bg-brown-texture btn-silver">Npcs</NuxtLink></div>
-          <div><NuxtLink to="/wiki/quests" class="btn-wiki-cat btn bg-brown-texture btn-silver">Quests</NuxtLink></div>
-          <div><NuxtLink to="/wiki/balance" class="btn-wiki-cat btn bg-brown-texture btn-silver">Balance</NuxtLink></div>
-          <div><NuxtLink to="/wiki/razas" class="btn-wiki-cat btn bg-brown-texture btn-silver">Razas</NuxtLink></div>
-          <div><NuxtLink to="/wiki/alimentos" class="btn-wiki-cat btn bg-brown-texture btn-silver">Alimentos</NuxtLink></div>
-          <div><NuxtLink to="/wiki/resistencia-magica" class="btn-wiki-cat btn bg-brown-texture btn-silver">Resistencia Magica</NuxtLink></div>
-          <div><NuxtLink to="/wiki/instrumentos" class="btn-wiki-cat btn bg-brown-texture btn-silver">Instrumentos Musicales</NuxtLink></div>
-          <div><NuxtLink to="/wiki/flechas-municiones" class="btn-wiki-cat btn bg-brown-texture btn-silver">Flechas y municiones</NuxtLink></div>
-          <div><NuxtLink to="/wiki/herramientas" class="btn-wiki-cat btn bg-brown-texture btn-silver">Herramientas</NuxtLink></div>
-          <div><NuxtLink to="/wiki/consumibles" class="btn-wiki-cat btn bg-brown-texture btn-silver">Consumibles</NuxtLink></div>
-          <div><NuxtLink to="/wiki/embarcaciones" class="btn-wiki-cat btn bg-brown-texture btn-silver">Navegacion</NuxtLink></div>
-          <div><NuxtLink to="/wiki/items-magicos" class="btn-wiki-cat btn bg-brown-texture btn-silver">Items Magicos</NuxtLink></div>
-          <div><NuxtLink to="/wiki/habilidades" class="btn-wiki-cat btn bg-brown-texture btn-silver">Habilidades</NuxtLink></div>
-          <div><NuxtLink to="/wiki/npcs-vendedores" class="btn-wiki-cat btn bg-brown-texture btn-silver">Npcs Vendedores</NuxtLink></div>
-          <div><NuxtLink to="/wiki/items-patreon" class="btn-wiki-cat btn bg-brown-texture btn-silver">Items Patreon</NuxtLink></div>
-          <div><NuxtLink to="/wiki/insignias" class="btn-wiki-cat btn bg-brown-texture btn-silver">Insignias</NuxtLink></div>
-          <div><NuxtLink to="/wiki/cofres" class="btn-wiki-cat btn bg-brown-texture btn-silver">Cofres</NuxtLink></div>
-          <div><NuxtLink to="/wiki/facciones" class="btn-wiki-cat btn bg-brown-texture btn-silver">Facciones</NuxtLink></div>
-          <div><NuxtLink to="/wiki/perdon" class="btn-wiki-cat btn bg-brown-texture btn-silver">Perdon</NuxtLink></div>
-          <div><NuxtLink to="/wiki/sastreria" class="btn-wiki-cat btn bg-brown-texture btn-silver">Sastreria</NuxtLink></div>
-          <div><NuxtLink to="/wiki/herreria" class="btn-wiki-cat btn bg-brown-texture btn-silver">Herreria</NuxtLink></div>
-          <div><NuxtLink to="/wiki/carpinteria" class="btn-wiki-cat btn bg-brown-texture btn-silver">Carpinteria</NuxtLink></div>
-          <div><NuxtLink to="/wiki/pescar" class="btn-wiki-cat btn bg-brown-texture btn-silver">Pescar</NuxtLink></div>
-          <div><NuxtLink to="/wiki/clanes" class="btn-wiki-cat btn bg-brown-texture btn-silver">Clanes</NuxtLink></div>
-          <div><NuxtLink to="/wiki/vender-en-mao" class="btn-wiki-cat btn bg-brown-texture btn-silver">Vender en MAO</NuxtLink></div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/escudos', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Escudos</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/armaduras', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Armaduras</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/armas', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Armas</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/cascos', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Cascos</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/monturas', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Monturas</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/hechizos', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Hechizos</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/npcs', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Npcs</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/quests', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Quests</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/balance', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Balance</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/razas', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Razas</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/alimentos', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Alimentos</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/resistencia-magica', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Resistencia Magica</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/instrumentos', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Instrumentos Musicales</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/flechas-municiones', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Flechas y municiones</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/herramientas', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Herramientas</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/consumibles', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Consumibles</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/embarcaciones', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Navegacion</NuxtLink>
+        </div>
+        <div>
+          <NuxtLink :to="{ path: '/wiki/items-magicos', query: { ...$route.query } }"
+            class="btn-wiki-cat btn bg-brown-texture btn-silver">Items Magicos</NuxtLink>
+        </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/habilidades', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Habilidades</NuxtLink>
       </div>
-      <!-- /Items container -->
-
-      <p class="text-center mb-8">Esta guia de entrenamiento es solo para darles una idea y ayudarlos en el camino, hay muchas variaciones de las mismas y no hay una forma correcta de entrenar a un personaje, cada cual lo puede hacer como guste.</p>
-      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full mb-6 content-rte text-xl gap-4">
-          <div><NuxtLink to="/wiki/guia/tips" class="btn-wiki-cat btn bg-brown-texture btn-silver">Antes de empezar</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/comenzar" class="btn-wiki-cat btn bg-brown-texture btn-silver">Comenzar a jugar</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/skills-clases" class="btn-wiki-cat btn bg-brown-texture btn-silver">Asignar skills por clases</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/minar" class="btn-wiki-cat btn bg-brown-texture btn-silver">¿Cómo y dónde minar?</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/leveleo-1-12" class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 1-12</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/leveleo-13-20" class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 13-20</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/leveleo-18-30" class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 18-30</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/leveleo-25-40" class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 25-40</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/leveleo-40-47" class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 40-47</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/leveleo-mapas-no-magicos" class="btn-wiki-cat btn bg-brown-texture btn-silver">Mapas NO Magicos</NuxtLink></div>
-          <div><NuxtLink to="/wiki/guia/leveleo-fans" class="btn-wiki-cat btn bg-brown-texture btn-silver">Video Guias Fans</NuxtLink></div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/npcs-vendedores', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Npcs Vendedores</NuxtLink>
       </div>
-
-      <p class="text-center mb-8">Soporte Tecnico.</p>
-      <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full mb-6 content-rte text-xl gap-4">
-          <div><NuxtLink to="/wiki/soporte/no-abre" class="btn-wiki-cat btn bg-brown-texture btn-silver">No abre el juego</NuxtLink></div>
-          <div><NuxtLink to="/wiki/soporte/no-abre-quests" class="btn-wiki-cat btn bg-brown-texture btn-silver">No abren Quests o Minimapa</NuxtLink></div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/items-patreon', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Items Patreon</NuxtLink>
       </div>
-    </section>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/insignias', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Insignias</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/cofres', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Cofres</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/facciones', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Facciones</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/perdon', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Perdon</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/sastreria', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Sastreria</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/herreria', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Herreria</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/carpinteria', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Carpinteria</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/pescar', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Pescar</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/clanes', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Clanes</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/vender-en-mao', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Vender en MAO</NuxtLink>
+      </div>
+    </div>
+    <!-- /Items container -->
+
+    <p class="text-center mb-8">Esta guia de entrenamiento es solo para darles una idea y ayudarlos en el camino, hay
+      muchas variaciones de las mismas y no hay una forma correcta de entrenar a un personaje, cada cual lo puede hacer
+      como guste.</p>
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full mb-6 content-rte text-xl gap-4">
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/tips', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Antes de empezar</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/comenzar', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Comenzar a jugar</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/skills-clases', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Asignar skills por clases</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/minar', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">¿Cómo y dónde minar?</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/leveleo-1-12', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 1-12</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/leveleo-13-20', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 13-20</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/leveleo-18-30', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 18-30</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/leveleo-25-40', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 25-40</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/leveleo-40-47', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Nivel 40-47</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/leveleo-mapas-no-magicos', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Mapas NO Magicos</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/guia/leveleo-fans', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">Video Guias Fans</NuxtLink>
+      </div>
+    </div>
+
+    <p class="text-center mb-8">Soporte Tecnico.</p>
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full mb-6 content-rte text-xl gap-4">
+      <div>
+        <NuxtLink :to="{ path: '/wiki/soporte/no-abre', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">No abre el juego</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink :to="{ path: '/wiki/soporte/no-abre-quests', query: { ...$route.query }} "
+          class="btn-wiki-cat btn bg-brown-texture btn-silver">No abren Quests o Minimapa</NuxtLink>
+      </div>
+    </div>
+  </section>
 
 
-  </div>
-</template>
+</div></template>
 
 <script>
 </script>
 
-<style>
-    .btn-wiki-cat {
-        @apply truncate px-4 w-full text-center text-sm normal-case text-yellow-400;
-    }
-</style>
+<style>.btn-wiki-cat {
+  @apply truncate px-4 w-full text-center text-sm normal-case text-yellow-400;
+}</style>
