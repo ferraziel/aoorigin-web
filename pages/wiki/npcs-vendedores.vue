7beb606a1ab0f03a8a217e3985fa40ce86a8f382
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-06-24 15:31:31 +1200

    Improved NPC Vendedores wiki page

--- pages/wiki/npcs-vendedores.vue
+++ pages/wiki/npcs-vendedores.vue
@@ -22,9 +22,9 @@
           <tbody>
             <tr v-for="npc in npcs" :key="npc.npc_id">
               <td class="text-right">{{ npc.npc_id }}</td>
-              <td>
-                <div>
-                  <img :alt="npc.NAME" :title="npc.NAME" :src="npc.canvasImage" width="64px" />
+              <td class="flex items-center">
+                <div class="flex items-center">
+                  <img :src="npc.canvasImage" :alt="npc.NAME" :title="npc.NAME">
                   <span class="text-right">{{ npc.NAME }}</span>
                 </div>
               </td>
@@ -32,14 +32,15 @@
               <td class="text-right">{{ npc.TIPOITEMSNAME }}</td>
               <td class="text-right">
                 <div v-if="npc.OBJSINFORMATION">
-                  <div v-for="item in npc.OBJSINFORMATION" :key="item.item_id">
-                    <img v-if="item.Data" :src="item.Data.canvasImage" :alt="item.Data.NAME" :title="item.Data.NAME" width="64px"/>
-                    <span v-if="item.Data">{{item.Data.NAME}}</span>
+                  <div v-for="item in npc.OBJSINFORMATION" :key="item.item_id" class="flex items-center">
+                    <img v-if="item.Data" :src="item.Data.canvasImage" :alt="item.Data.NAME" :title="item.Data.NAME" class="w-12 h-12 mr-2 rounded-full">
+                    <span v-if="item.Data">{{ item.Data.NAME }}</span>
                   </div>
                 </div>
               </td>
             </tr>
           </tbody>
+
         </table>
       </div>
     </div>
