fbb1596292b20da9e15ca38415fc74e557978a5b
Lucas Recoaro <lucas.recoaro@gmail.com>
2023-07-08 15:36:27 +1200

    Added null reference check to formatPrice

--- mixins/priceMixin.js
+++ mixins/priceMixin.js
@@ -1,6 +1,7 @@
 export const priceMixin = {
   methods: {
     formatPrice(price, currency) {
+      if (!price) return '';
       return price.toLocaleString('es-AR', {
         style: 'currency',
         currency: currency,
